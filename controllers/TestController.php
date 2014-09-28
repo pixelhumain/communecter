dac2e156672c70cbcf2881b7c7d734b35827587d
Tibor Katelbach <oceatoon@gmail.com>
2014-09-28 14:28:15 +0400

    test controller

---
+++ controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ActionLocaleController.php
+ *
+ * tous ce que propose le PH en terme de projet
+ * comment agir localeement
+ *
+ * @author: Tibor Katelbach <tibor@pixelhumain.com>
+ * Date: 15/08/13
+ */
+class TestController extends Controller {
+    const moduleTitle = "Test";
+    public function actionIndex() 
+    {
+	    echo "test";
+	}
+   
+}
