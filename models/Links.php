b24b3f97658c2206f5176424341dff44b531a835
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-03-04 18:17:43 +0400

    Change function signature for Links

--- models/Links.php
+++ models/Links.php
@@ -1,7 +1,10 @@
 <?php
 class Links {
 	
-	/** 
+	const MEMBER_TYPE_PERSON 			= "person";
+	const MEMBER_TYPE_ORGANIZATION 		= "organization";
+	
+	/**
 	 * Add a member to an organization
 	 * Create a link between the 2 actors. The link will be typed members and memberOf
 	 * The memberOf should be an organization
@@ -9,37 +12,58 @@ class Links {
 	 * 2 entry will be added :
 	 * - $memberOf.link.members["$memberId"]
 	 * - $member.link.memberOf["$memberOfId"]
-	 * @param type $memberOf The memberOf (organization) where a member will be linked.
-	 * @param type $member The member could be an organization or a person. It will be the member added to the memberOf
-	 * @param type $userId The userId doing the action
+	 * @param type $memberOfId The Id memberOf (organization) where a member will be linked. 
+	 * @param type $memberOfType The Type (should be organization) memberOf where a member will be linked. 
+	 * @param type $memberId The member Id to add. It will be the member added to the memberOf
+	 * @param type $memberType MemberType to add : could be an organization or a person
+	 * @param type $userId $userId The userId doing the action
 	 * @return result as Json with the result of the operation
 	 */
-    public static function addMember($memberOf, $member, $userId) {
+    public static function addMember($memberOfId, $memberOfType, $memberId, $memberType, $userId) {
         
         //0. Check if the $memberOf and the $member exists
+        if ($memberOfType == MEMBER_TYPE_ORGANIZATION) {
+        	$organization = Organization::getById($memberOfId); 
+        }
         //1. Check if the $userId can manage the $memberOf
         //2. Create the links
         //3. Send Notifications
 
-        $result = array('' => , );
+        $result = array();
        
         return $result;
     }
 
-    /** 
-	 * Connect 2 actors : organization or Person
+    /**
+     * Connect 2 actors : organization or Person
 	 * Create a link between the 2 actors. The link will be typed as knows
 	 * 1 entry will be added :
 	 * - $origin.link.knows["$target"]
-	 * The $target will receive a notification saying that the $origin is now linked with him
-	 * @param type $origin The organization or Personn who wants to create a link with the $target
-	 * @param type $target The organization or Person that will be linked
-	 * @param type $userId The userId doing the action
-	 * @return result as Json with the result of the operation
-	 */
-    public static function connect($origin, $target, $userId) {
+     * @param type $originId The Id of actor who wants to create a link with the $target
+     * @param type $originType The Type (Organization or Person) of actor who wants to create a link with the $target
+     * @param type $targetId The actor that will be linked
+     * @param type $targetType The Type (Organization or Person) that will be linked
+     * @param type $userId The userId doing the action
+     * @return result as Json with the result of the operation
+     */
+    public static function connect($originId, $originType, $targetId, $targetType, $userId) {
         
-        $result = array('' => , );
+        $result = array();
+       
+        return $result;
+    }
+
+    /**
+     * Check if two actors are connected with a link knows
+     * @param type $originId The Id of actor to check the link with the $target
+     * @param type $originType The Type (Organization or Person) of actor to check the link with the $target
+     * @param type $targetId The actor to check that is linked
+     * @param type $targetType The Type (Organization or Person) to check that is linked
+     * @return boolean : true if the actors are connected, false else
+     */
+    public static function isConnected($originId, $originType, $targetId, $targetType) {
+        
+        $result = array();
        
         return $result;
     }
@@ -51,14 +75,16 @@ class Links {
 	 * 1 entry will be added :
 	 * - $invitor.link.knows["$guest"] = "status = toBeValidated"
 	 * One Person or Organization will be created with basic information
-	 * @param type $invitor The organization or Personn who invite a guest
-	 * @param type $guest The organization or Person that will invited
+	 * @param type $invitorId The actor Id who invite a guest
+	 * @param type $invitorType The type (organization or person) who invite the guest
+	 * @param type $guestId The actor Id that will invited
+	 * @param type $guestType The type (organization or person) that will invited
 	 * @param type $userId The userId doing the action
 	 * @return result as Json with the result of the operation
 	 */
-    public static function invite($invitor, $guest, $userId) {
+    public static function invite($invitorId, $invitorType, $guestId, $guestType, $userId) {
  
-        $result = array('' => , );
+        $result = array();
        
         return $result;
     }
