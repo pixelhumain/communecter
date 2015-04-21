7766831a9dd5fd697ae6b6fdd716ee5bb61bf04d
Robert Johnson <zeriam974@gmail.com>
2015-04-21 15:51:35 +0400

    space bouton after dynamycally add

--- views/organization/dashboard/network.php
+++ views/organization/dashboard/network.php
@@ -179,10 +179,10 @@
           							'<td>'+roles+'</td>'+
       								'<td class="center">'+
 										'<div class="visible-md visible-lg hidden-sm hidden-xs">'+
-											'<a href="'+links+'" class="btn btn-xs btn-light-blue tooltips " data-placement="top" data-original-title="View"><i class="fa fa-search"></i> </a>'+
-											'<a  href="" class="btn btn-xs btn-light-blue tooltips " data-placement="top" data-original-title="Edit"><i class="fa fa-pencil-square-o"></i> </a>'+
-											'<a href="javascript:;" class="disconnectBtnNet btn btn-xs btn-red tooltips " data-linkType=""  data-type="'+type+'" data-name="'+newOrga.name+'" data-placement="top" data-original-title="" ><i class=" disconnectBtnIcon fa fa-unlink"></i> </a>'+
-										'</div>'+
+											' <a href="'+links+'" class="btn btn-xs btn-light-blue tooltips " data-placement="top" data-original-title="View"><i class="fa fa-search"></i> </a>'+
+											' <a  href="" class="btn btn-xs btn-light-blue tooltips " data-placement="top" data-original-title="Edit"><i class="fa fa-pencil-square-o"></i> </a>'+
+											' <a href="javascript:;" class="disconnectBtnNet btn btn-xs btn-red tooltips " data-linkType=""  data-type="'+type+'" data-name="'+newOrga.name+'" data-placement="top" data-original-title="" ><i class=" disconnectBtnIcon fa fa-unlink"></i> </a>'+
+										' </div>'+
 									'</td>'+
         						'</tr>';
 
