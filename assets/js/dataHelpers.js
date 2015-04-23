25d4aaf0347a67dfe66c099b1e80c2e583a1ad78
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-04-23 13:47:11 +0400

    dataHelpers : to load select and select2 with ajax

---
+++ assets/js/dataHelpers.js
@@ -0,0 +1,69 @@
+//Retrieve the countries in ajax and return an array of value
+//The selectType change the value key : 
+//for select input : {"value":"FR", "text":"France"}
+//for select2 input : {"id":"FR", "text":"France"}
+function getCountries(selectType) {
+	var result = new Array();
+	$.ajax({
+		url: baseUrl+'/'+moduleId+"/api/getCountries",
+		type: 'post',
+		global: false,
+		async: false,
+		dataType: 'json',
+		success: function(data) {
+			$.each(data, function(i,value) {
+				if (selectType == "select2") {
+					result.push({"id" : value.value, "text" :value.text});
+				} else {
+					result.push({"value" : value.value, "text" :value.text});
+				}
+			}) 
+		}
+	});
+	return result;
+}
+
+//Retrieve the publics type in ajax and return an array of value
+function getPublics(selectType) {
+	var result =new Array();
+	$.ajax({
+		url: baseUrl+'/'+moduleId+"/datalist/getlistbyname/name/public",
+		type: 'post',
+		global: false,
+		async: false,
+		dataType: 'json',
+		success: function(data) {
+			console.log("Data list :"+data.list)
+			$.each(data.list, function(i,value) {
+				if (selectType == "select2") {
+					result.push({"id" : value.value, "text" :value.text});
+				} else {
+					result.push({"value" : value.value, "text" :value.text});
+				}
+			})
+		}
+	});
+	return result;
+}
+
+function getCitiesByPostalCode(postalCode, selectType) {
+	var result =new Array();
+	$.ajax({
+		url: baseUrl+'/'+moduleId+"/api/getcitiesbypostalcode/",
+		data: {postalCode: postalCode},
+		type: 'post',
+		global: false,
+		async: false,
+		dataType: 'json',
+		success: function(data) {
+			$.each(data, function(i,value) {
+				if (selectType == "select2") {
+					result.push({"id" : value.value, "text" :value.text});
+				} else {
+					result.push({"value" : value.value, "text" :value.text});
+				}
+			});
+		}
+	});
+	return result;
+}
