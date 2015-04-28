310523125158c3d1f961c58726d961a4d29d10ba
Robert Johnson <zeriam974@gmail.com>
2015-04-28 12:27:16 +0400

    Upload images save and update image profil

--- controllers/DocumentController.php
+++ controllers/DocumentController.php
@@ -17,4 +17,27 @@ class DocumentController extends CommunecterController {
 	public function actionSave() {
 	      return Rest::json( Document::save($_POST) );
 	}
+
+	/**
+	* delete a document
+	* @param $id id of the document that we went to delete
+	*/
+	public function actionDeleteDocumentById($id){
+		return Rest::json( Document::removeDocumentById($id));
+	}
+
+	/**
+	*
+	*
+	*/
+
+	public function actionRemoveAndBacktract(){
+		$result = array("result"=>false,"msg"=>"Vos données n'ont pas pu être modifier");
+		if(isset($_POST["_id"])){
+			Document::removeDocumentById($_POST["_id"]);
+			Document::setImagePath($_POST["id"], $_POST["type"], $_POST["imagePath"], $_POST["contentKey"]);
+			$result = array("result"=>true,"msg"=>"Vos données ont bien été modifier");
+		}
+		return Rest::json($result);
+	}
 }
--- views/pod/sliderAgenda.php
+++ views/pod/sliderAgenda.php
@@ -1,6 +1,7 @@
 <style type="text/css">
 	.flexslider{
 		margin : 0px 0px 0px;
+		height: 220px;
 	}
 	#sliderAgenda .flex-control-nav{
 		opacity: 0;
@@ -45,25 +46,27 @@ var events = <?php echo json_encode($events) ?>;
 		
 		if(Object.keys(events).length>0){
 			$.each(events, function(k, v){
-				var period = getStringPeriodValue(v.startDate, v.endDate);
-				var date = new Date(v.endDate.split("/")[2].split(" ")[0], parseInt(v.endDate.split("/")[1])-1, v.endDate.split("/")[0]);
-				if(n<4 && compareDate(today, date)){
-					notEmptySlide = true;
-					if (typeof(v.imagePath)=="undefined"){
-						v.imagePath = "http://placehold.it/350x180";
+				if(typeof v.startDate != "undefined" && typeof v.endDate !="undefined"){
+					var period = getStringPeriodValue(v.startDate, v.endDate);
+					var date = new Date(v.endDate.split("/")[2].split(" ")[0], parseInt(v.endDate.split("/")[1])-1, v.endDate.split("/")[0]);
+					if(n<4 && compareDate(today, date)){
+						notEmptySlide = true;
+						if (typeof(v.imagePath)=="undefined"){
+							v.imagePath = "http://placehold.it/350x180";
+						}
+						var htmlRes = "<li><div>"+
+										"<img src='"+v.imagePath+"'></img>";
+						htmlRes +="<div class='row'>"+
+									"<div class='col-xs-5' >"+
+										"<h2>"+period+"</h2></div>";
+						htmlRes += "<div class='col-xs-7' >"+
+										"<h1>"+v.name+"</h1>"+
+										"<div id='infoEventLink'>"+
+											"<a href='"+baseUrl + "/" + moduleId + "/event/dashboard/id/"+v["_id"]["$id"]+"''>En savoir+ <i class='fa fa-angle-right'></i> </a>"+
+										"</div></div></div></li>";
+						$("#slidesAgenda").append(htmlRes);
+						n++;
 					}
-					var htmlRes = "<li><div>"+
-									"<img src='"+v.imagePath+"'></img>";
-					htmlRes +="<div class='row'>"+
-								"<div class='col-xs-5' >"+
-									"<h2>"+period+"</h2></div>";
-					htmlRes += "<div class='col-xs-7' >"+
-									"<h1>"+v.name+"</h1>"+
-									"<div id='infoEventLink'>"+
-										"<a href='"+baseUrl + "/" + moduleId + "/event/dashboard/id/"+v["_id"]["$id"]+"''>En savoir+ <i class='fa fa-angle-right'></i> </a>"+
-									"</div></div></div></li>";
-					$("#slidesAgenda").append(htmlRes);
-					n++;
 				}
 			})	
 		}
