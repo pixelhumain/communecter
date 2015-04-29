eb0ecbe35893ab2fff9e69a349c3451ca8a2a100
Robert Johnson <zeriam974@gmail.com>
2015-04-29 14:12:56 +0400

    fix bug of all dashboard

--- views/organization/dashboard1.php
+++ views/organization/dashboard1.php
@@ -25,10 +25,36 @@
 		</div>
 	</div>
 	<div class="col-sm-4 col-xs-12">
-		<?php $this->renderPartial('../pod/photoVideo',array( "context" => (isset($organization)) ? $organization : null )); ?>
+		<div class="col-sm-12 col-xs-12 photoVideoPod">
+ 			<div class="panel panel-white pulsate">
+				<div class="panel-heading border-light ">
+					<h4 class="panel-title"> <i class='fa fa-cog fa-spin fa-2x icon-big text-center'></i> Loading Media Section</h4>
+					<div class="space5"></div>
+				</div>
+			</div>
+ 			
+ 		</div>
 	</div>
 </div>
 
 <script type="text/javascript">
 	var contextMap= <?php echo json_encode($contextMap) ?>;
+	var images = <?php echo json_encode($images) ?>;
+	var contentKeyBase = "<?php echo $contentKeyBase ?>";
+	jQuery(document).ready(function() {
+		if($(".tooltips").length) {
+     		$('.tooltips').tooltip();
+   		}
+		$('.pulsate').pulsate({
+            color: '#2A3945', // set the color of the pulse
+            reach: 10, // how far the pulse goes in px
+            speed: 1000, // how long one pulse takes in ms
+            pause: 200, // how long the pause between pulses is in ms
+            glow: false, // if the glow should be shown too
+            repeat: 10, // will repeat forever if true, if given a number will repeat for that many times
+            onHover: false // if true only pulsate if user hovers over the element
+        });
+
+		getAjax(".photoVideoPod", baseUrl+"/"+moduleId+"/pod/photovideo/id/<?php echo $_GET["id"]?>/type/<?php echo Organization::COLLECTION ?>", function(){bindPhotoSubview();}, "html");
+	});
 </script>
