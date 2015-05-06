7c0c5eda3d94bf9ca0dad91b2f858a9598b8d7d1
Robert Johnson <zeriam974@gmail.com>
2015-05-06 16:45:08 +0400

    gallery confirm

--- controllers/DocumentController.php
+++ controllers/DocumentController.php
@@ -38,11 +38,11 @@ class DocumentController extends CommunecterController {
 	}
 
 	public function actionRemoveAndBacktract(){
-		$result = array("result"=>false,"msg"=>"Vos données n'ont pas pu être modifier");
+		$result = array("result"=>false,"msg"=>"Vos données n'ont pas pu être modifiées");
 		if(isset($_POST["_id"])){
 			Document::removeDocumentById($_POST["_id"]);
 			Document::setImagePath($_POST["id"], $_POST["type"], $_POST["imagePath"], $_POST["contentKey"]);
-			$result = array("result"=>true,"msg"=>"Vos données ont bien été modifier");
+			$result = array("result"=>true,"msg"=>"Vos données ont bien été modifiées");
 		}
 		return Rest::json($result);
 	}
