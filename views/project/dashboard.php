0c18ebc2355c1327ad1eba66616bd3f69e2c9da0
Tibor Katelbach <oceatoon@gmail.com>
2015-07-06 20:43:03 +0400

    added timesheet include

--- views/project/dashboard.php
+++ views/project/dashboard.php
@@ -1,3 +1,4 @@
+
 <div class="row">
 	<div class="col-lg-4 col-md-12">
 		<?php 
@@ -12,16 +13,19 @@
 		 <?php $this->renderPartial('dashboard/contributors',array( "contributors" => $contributors, "organizationTypes" => $organizationTypes, "project" => $project, "admin" => $admin)); ?>
 	</div>
 	<div class="row">
-	<?php if (!empty($tasks) OR $admin==true){ ?>
-	<div class ="col-lg-8 col-md-8">
-		 <?php $this->renderPartial('dashboard/timesheet',array("itemId" => (string)$project["_id"], "tasks" => $tasks, "admin" =>$admin)); ?>
+		<?php if (!empty($tasks) OR $admin==true){ ?>
+		<div class ="col-lg-8 col-md-8">
+			 <?php $this->renderPartial('dashboard/timesheet',array("itemId" => (string)$project["_id"], "tasks" => $tasks, "admin" =>$admin)); ?>
+		</div>
+		<?php } 
+		 if (!empty($properties) OR $admin==true){ ?>
+		<div class ="col-lg-4 col-md-12">
+			 <?php $this->renderPartial('dashboard/projectChart',array("itemId" => (string)$project["_id"], "properties" => $properties, "admin" =>$admin)); ?>
+		</div>
+		<?php } ?>
 	</div>
-	<?php } 
-	 if (!empty($properties) OR $admin==true){ ?>
-	<div class ="col-lg-4 col-md-12">
-		 <?php $this->renderPartial('dashboard/projectChart',array("itemId" => (string)$project["_id"], "properties" => $properties, "admin" =>$admin)); ?>
-	</div>
-	<?php } ?>
+	<div class="row" id="timesheetphp">
+	<?php echo Yii::import('ext.timesheetphp.sources.timesheet', true); ?>
 	</div>
 </div>
 <?php $this->renderPartial('/sig/generic/mapLibs'); ?>
