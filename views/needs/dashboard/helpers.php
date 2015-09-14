4f2c109506fb097cfdf31e0385c871a8b2b404ee
Clément Damiens <clement.damiens@gmail.com>
2015-09-14 11:38:32 +0200

    Add helper (engaged and validation)

--- views/needs/dashboard/helpers.php
+++ views/needs/dashboard/helpers.php
@@ -79,89 +79,83 @@
 				</ul>-->
 				
 				<div class="tab-content partition-white">
-					<?php //for ($i=0; $i<$quantity;$i++){ ?>
-						<div class="col-md-3 col-xs-4 center padding-10 helperBox">
-							<img width="50" height="50" alt="image" class="img-circle" src="/pixelhumain/ph/communecter/document/resized/50x50/upload/communecter/citoyens/555a124b126e9a6f6600000d/famillesuricate4.jpg">
-						</div>
-						<div class="col-md-3 col-xs-4 center padding-10 helperBox">
-							<a href="#" class="refuseHelp">
-								<i class="fa fa-times-circle refuseBtn confirmBtn"></i>
-							</a>
-							<img width="50" height="50" alt="image" class="img-circle grayscale" src="/pixelhumain/ph/communecter/document/resized/50x50/upload/communecter/citoyens/555a124b126e9a6f6600000d/famillesuricate4.jpg">
-							<a href="#" class="acceptHelp">
-								<i class="fa fa-check-circle validateBtn confirmBtn"></i>
-							</a>
-						</div>
-						<div class="col-md-3 col-xs-4 center padding-10 helperBox">
-							<!--<img width="50" height="50" alt="image" class="img-circle grayscale" src="/pixelhumain/ph/communecter/document/resized/50x50/upload/communecter/citoyens/555a124b126e9a6f6600000d/famillesuricate4.jpg">-->
+					<?php
+					$nbValidate=0; 
+					if(!empty($helpers)){
+						$i=0;
+						foreach ($helpers as $id => $val){
+							if($val["isValidated"] == 1){ ?>
+								<div class="col-md-3 col-xs-4 center padding-10 helperBox">
+								<?php if(isset($val["imagePath"])) { ?>
+									<img width="50" height="50" alt="image" class="img-circle" src="<?php echo Yii::app()->createUrl('/'.$this->module->id.'/document/resized/50x50'.$val['imagePath']) ?>">
+								<?php } else{ 
+									echo "<i class=\"fa fa-smile-o fa-2x\"></i>";
+								} ?>
+								</div>
+							<?php 
+								$nbValidate++;	
+							}
+							else{ ?>
+								<div class="col-md-3 col-xs-4 center padding-10 helperBox">
+									<a href="javascript:;" class="refuseHelp">
+										<i class="fa fa-times-circle refuseBtn confirmBtn"></i>
+									</a>
+									<a href="<?php echo Yii::app()->createUrl("/".$this->module->id."/person/dashboard/id/".(string)$val["_id"])?>">
+									<?php if(isset($val["imagePath"])) { ?>
+									<img width="50" height="50" alt="image" class="img-circle grayscale" src="<?php echo Yii::app()->createUrl('/'.$this->module->id.'/document/resized/50x50'.$val['imagePath']) ?>">
+									<?php } else{ 
+												echo "<i class=\"fa fa-smile-o fa-2x\"></i>";
+											} ?>
+									<a href="javascript:;" class="acceptHelp" data-id="<?php echo (string)$val["_id"]; ?>" data-name="<?php echo $val["name"]; ?>">
+										<i class="fa fa-check-circle validateBtn confirmBtn"></i>
+									</a>
+								</div>
+								
+							<?php }
+							$i++;
+						}
+						if($i != $quantity){
+							for ($i;$i<$quantity;$i++){ ?>
+								<div class="col-md-3 col-xs-4 center padding-10 helperBoxEmpty">
+									<div class="img-circle" style="height: 50px;width: 50px;border: 3px solid gray;margin:auto;">
+										<p style="line-height: 45px; font-size: 25px;"> ? </p>
+									</div>
+								</div>
+							<?php }
+						}
+					}
+					else{
+						for ($i=0; $i<$quantity;$i++){ ?>
+						<div class="col-md-3 col-xs-4 center padding-10 helperBoxEmpty">
 							<div class="img-circle" style="height: 50px;width: 50px;border: 3px solid gray;margin:auto;">
 	    	<p style="line-height: 45px; font-size: 25px;"> ? </p>
 							</div>
-
 						</div>
-					<?php // } ?>
+					<?php }
+						} ?>
+
 					<div class="col-md-3 col-xs-4 center padding-10">
-						<strong style="font-size:25px;color:#27b3e2;">1</strong> / <strong>3</strong> HELPERS
+						<strong style="font-size:25px;color:#27b3e2;"><?php echo $nbValidate; ?></strong> / <strong><?php echo $quantity;?></strong> HELPERS
 					</div>
 					<div class="col-md-12 center"> 
 						<p style="font-variant: small-caps;font-size: 15px;font-style: italic;"> Contribuez au projet en répondant à ce besoin !! </p>
 					</div>
 					<div class="col-md-12 center padding-10"> 
-						<a class="new-proposal btn btn-info" href="#newProposal"></i> Se proposer </a>
+						<a href="javascript:;" class="new-proposal btn btn-info"></i> Se proposer </a>
 					</div>
-					<?php if (isset($helpers)){ ?>
-					<div class="tab-pane padding-bottom-5 active" id="users_tab_attending">
-						<div class="panel-scroll height-230 ps-container">
-							<table class="table table-striped table-hover">
-								<tbody id="tContributor">
-									<?php
-										foreach ($contributors as $member) { ?>
-									<tr id="contributor<?php echo $member["_id"]; ?>">
-										<td class="center">
-										<?php if($member && isset($member["imagePath"])) { ?>
-											<img width="50" height="50"  alt="image" class="img-circle" src="<?php echo $member["imagePath"]; ?>"></td>
-										<?php } else{ 
-												if ($member["type"]=="citoyen"){?>
-													<i class="fa fa-smile-o fa-2x"></i></td>
-												<?php }else{ ?>
-													<i class="fa fa-group fa-2x"></i></td>
-										<?php	} 
-											} ?>
-										<td><span class="text-small block text-light"><?php if ($member && isset($member["position"])) echo $member["position"]; ?></span><span class="text-large"><?php echo $member["name"]; ?></span><a href="<?php echo Yii::app()->createUrl("/".$this->module->id."/person/dashboard/id/".$member['_id'])?>" class="btn"><i class="fa fa-chevron-circle-right"></i></a></td>
-										<?php if ( $admin ){ ?>
-											<td>
-												<a href="javascript:;" class="disconnectBtnContributor btn btn-xs btn-red tooltips " data-placement="left"  data-type="<?php if ($member["type"]=="citoyen") echo PHType::TYPE_CITOYEN; else echo  Organization::COLLECTION; ?>" data-id="<?php echo $member['_id'];?>" data-name="<?php echo $member["name"]; ?>" data-placement="top" data-original-title="Remove this organization" ><i class=" disconnectBtnIcon fa fa-unlink"></i></a>
-											</td>
-										<?php } ?>
-									</tr>
-									<?php } 
-																			?>
-								</tbody>
-							</table>
-						<div class="ps-scrollbar-x-rail" style="left: 0px; bottom: -14px; width: 504px; display: none;"><div class="ps-scrollbar-x" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 17px; right: 3px; height: 230px; display: inherit;"><div class="ps-scrollbar-y" style="top: 11px; height: 152px;"></div></div></div>
-					</div>
-					<?php } else{ ?>
-					<!--<div id="infoPodOrga" class="padding-10">
-						<blockquote> 
-							Create needs
-							<br>Materials  
-							<br>Knowledge
-							<br>Skills
-							<br>to call ressources that you need
-						</blockquote>
-					</div>-->		
-					<?php	} ?>
 				</div>
 			</div>
 		</div>
 	</div>
 <script type="text/javascript">
 admin= <?php echo $admin; ?>;
+needId= "<?php echo $id; ?>";
 jQuery(document).ready(function(){
 	if (admin==1){
 		$(".helperBox").mouseenter(function(){
 			if($(this).find(".confirmBtn").length){
 				$(this).find(".confirmBtn").fadeIn();
+				acceptHelp();
 			}
 		}).mouseleave(function(){
 			if($(this).find(".confirmBtn").length){
@@ -169,5 +163,61 @@ jQuery(document).ready(function(){
 			}			
 		});
 	}
+	helpProposal();
 });
+function helpProposal(){
+$(".new-proposal").off().on("click",function () {
+	        //$(".disconnectBtnIcon").removeClass("fa-unlink").addClass("fa-spinner fa-spin");
+	        bootbox.confirm("Are you sure you want to help for this need <span class='text-red'><?php echo $name; ?></span>?", 
+			function(result) {
+					if (result) {
+						alert(result);
+						$.ajax({
+					        type: "POST",
+					        url: baseUrl+"/"+moduleId+"/needs/addhelpervalidation/needId/"+needId+"/booleanState/0/",
+					       	dataType: "json",
+				        	success: function(data){
+					        	console.log(data);
+					        	if ( data && data.result ) {               
+						       	 	toastr.info("HELP SUCCESFULLY ADD!! WAIT fOR VALIDATION");
+						       	 	//addAttentConfirmBox();
+						        } else {
+						           toastr.info(data.msg);
+						          // $(".disconnectBtnIcon").removeClass("fa-spinner fa-spin").addClass("fa-unlink");
+						        }
+						    }
+						});
+					}
+				}
+			)
+		});
+}
+function acceptHelp(){
+$(".acceptHelp").off().on("click",function () {
+	var idHelper = $(this).data("id");
+	        //$(".disconnectBtnIcon").removeClass("fa-unlink").addClass("fa-spinner fa-spin");
+	        bootbox.confirm("Are you sure you want the help help <span class='text-red'>"+$(this).data("name")+"</span>?", 
+			function(result) {
+					if (result) {
+						alert(idHelper);
+						$.ajax({
+					        type: "POST",
+					        url: baseUrl+"/"+moduleId+"/needs/addhelpervalidation/needId/"+needId+"/helperId/"+idHelper+"/booleanState/1/",
+					       	dataType: "json",
+				        	success: function(data){
+					        	console.log(data);
+					        	if ( data && data.result ) {               
+						       	 	toastr.info("HELP IS VALIDATED");
+						       	 	//addAttentConfirmBox();
+						        } else {
+						           toastr.info("something went wrong!! please try again.");
+						          // $(".disconnectBtnIcon").removeClass("fa-spinner fa-spin").addClass("fa-unlink");
+						        }
+						    }
+						});
+					}
+				}
+			)
+		});
+}
 </script>
