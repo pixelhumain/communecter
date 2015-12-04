1a8e855b39ccb707ba3eb2b6ee5bbfa3ed2cd249
Tibor Katelbach <oceatoon@gmail.com>
2015-12-04 06:35:26 +0100

    auth

--- components/CommunecterController.php
+++ components/CommunecterController.php
@@ -356,11 +356,11 @@ class CommunecterController extends Controller
     if (true)//(isset($_SERVER["HTTP_ORIGIN"]) )//&& $_SERVER["REMOTE_ADDR"] == "52.30.32.155" ) //this is an outside call 
     { 
       $host = "meteor.communecter.org";
-      if (strpos("http://".$host, $_SERVER["HTTP_ORIGIN"]) >= 0 || strpos("https://".$host, $_SERVER["HTTP_ORIGIN"]) >= 0 ){
+      //if (strpos("http://".$host, $_SERVER["HTTP_ORIGIN"]) >= 0 || strpos("https://".$host, $_SERVER["HTTP_ORIGIN"]) >= 0 ){
         if( isset( $_SERVER["X-Auth-Token"]) && Authorisation::isMeteorConnected( $_SERVER["X-Auth-Token"] ) ){
           $prepareData = false;
         }
-      } 
+      //} 
     } else if( (!isset( $page["public"] ) )
       && !in_array(Yii::app()->controller->id."/".Yii::app()->controller->action->id, $pagesWithoutLogin)
       && !Yii::app()->session[ "userId" ] )
