5e6b5fb67175b45f0dfd68c993ef7873a989fa34
Kgneo <tristan.goguet@gmail.com>
2016-01-19 15:59:44 +1100

    move loadByHash() to communecter.js

--- views/person/login.php
+++ views/person/login.php
@@ -8,7 +8,9 @@ $cs->registerScriptFile($this->module->assetsUrl. '/js/dataHelpers.js' , CClient
 $cs->registerScriptFile($this->module->assetsUrl. '/js/sig/localisationHtml5.js' , CClientScript::POS_END);
 //geolocalisation nominatim et byInsee
 $cs->registerScriptFile($this->module->assetsUrl. '/js/sig/geoloc.js' , CClientScript::POS_END);
-	
+
+//
+$cs->registerScriptFile($this->module->assetsUrl. '/js/communecter.js' , CClientScript::POS_END);	
 ?>
 <style type="text/css">
 .container-lbl-info-search{
@@ -1452,73 +1454,6 @@ function autoCompleteSearch(name){
   }
 
   
-function loadByHash( hash , back) { 
-    console.log("loadByHash",hash);
-
-    params = ( hash.indexOf("?") < 0 ) ? '?tpl=directory2&isNotSV=1' : "";
-    if( hash.indexOf("#person.directory") >= 0 )
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+params, 'PERSON DIRECTORY ','share-alt' );
-    else if( hash.indexOf("#organization.directory") >= 0 )
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+params, 'ORGANIZATION MEMBERS ','users' );
-    else if( hash.indexOf("#project.directory") >= 0 )
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+params, 'PROJECT CONTRIBUTORS ','users' );
-    else if( hash.indexOf("#panel") >= 0 ){
-        if(hash.substr(7) == "box-add")
-            title = 'ADD SOMETHING TO MY NETWORK';
-        else
-            title = "WELCOM MUNECT HEY !!!";
-        showPanel(hash.substr(7),null,title);
-    }
-    
-    else if( hash.indexOf("#person.detail") >= 0 )
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+params, 'PERSON DETAIL ','user' );
-    else if( hash.indexOf("#event.detail") >= 0 )
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+params, 'EVENT DETAIL ','calendar' );
-    else if( hash.indexOf("#project.detail") >= 0 )
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+params, 'PROJECT DETAIL ','lightbulb-o' );
-    else if( hash.indexOf("#organization.detail") >= 0 )
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+params, 'ORGANIZATION DETAIL ','users' );
-    else if( hash.indexOf("#city.detail") >= 0 )
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+params, 'CITY ','university' );
-    
-    else if( hash.indexOf("#organization.addorganizationform") >= 0 )
-        showAjaxPanel( '/organization/addorganizationform?isNotSV=1', 'ADD AN ORGANIZATION','users' );
-    else if( hash.indexOf("#person.invitesv") >= 0 )
-        showAjaxPanel( '/person/invitesv?isNotSV=1', 'INVITE SOMEONE','share-alt' );
-    else if( hash.indexOf("#event.eventsv") >= 0 )
-        showAjaxPanel( '/event/eventsv?isNotSV=1', 'ADD AN EVENT','calendar' );
-    else if( hash.indexOf("#project.projectsv") >= 0 )    
-        showAjaxPanel( '/project/projectsv/id/<?php echo Yii::app()->session['userId']?>/type/citoyen?isNotSV=1', 'ADD A PROJECT','lightbulb-o' );
-    else if( hash.indexOf("#project.addcontributorsv") >= 0 )    
-        showAjaxPanel( '/project/projectsv/id/<?php echo Yii::app()->session['userId']?>/type/citoyen?isNotSV=1', 'ADD A PROJECT','lightbulb-o' );
-
-    else if( hash.indexOf("#rooms.index.type") >= 0 ){
-        hashT = hash.split(".");
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+'?&isNotSV=1', 'ACTIONS in this '+typesLabels[hashT[3]],'rss' );
-    } else if ( hash.indexOf("#survey.entry.id") >= 0 ) {
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+'?&isNotSV=1', 'VOTE LOCAL ','legal' );
-    }  else if ( hash.indexOf("#rooms") >= 0 ) {
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+'?&isNotSV=1', 'ACTION ROOMS ','cubes' );
-    }   
-
-    else if ( hash.indexOf("#admin.importdata") >= 0 ) {
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+'?&isNotSV=1', 'IMPORT DATA ','download' );
-    }   
-
-    else if( hash.indexOf("#news.index.type") >= 0 ){
-        hashT = hash.split(".");
-        showAjaxPanel( '/'+hash.replace( "#","" ).replace( /\./g,"/" )+'?&isNotSV=1', 'KESS KISS PASS in this '+typesLabels[hashT[3]],'rss' );
-    } else if(userId != "")
-        showAjaxPanel( '/news?isNotSV=1', 'KESS KISS PASS ','rss' );
-    else
-        showPanel('box-communecter',null,"WELCOM MUNECT HEY !!!",null);
-
-    location.hash = hash;
-    if( !back )
-      history.pushState( { "hash" :hash} , null, hash );
-    console.warn("pushState",hash);
-
-}
 
 
 </script>
