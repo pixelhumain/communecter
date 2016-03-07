cdc623e59dc598e3d161397e0477db59e2568b7c
raphael <raphael_riviere@yahoo.fr>
2016-03-07 16:58:09 +0400

    Modification de l'interface de sourceAdmin

--- views/admin/sourceadmin.php
+++ views/admin/sourceadmin.php
@@ -4,8 +4,40 @@
 	</div>
 	<div class="panel-body">
 		<h4 class="panel-title">Entité mal </h4>
+		<br/>
 		<table id="tableEntity" class="col-sm-12 col-xs-12">
-			
+			<tr>
+				<th>Type</th>
+				<th>Name</th>
+				<th>Warnings</th>
+			</tr>
+			<?php 
+				$chaine = "";
+				//var_dump($entities);
+				foreach ($entities as $keyEntities => $valueEntities) {
+					//var_dump(count($valueEntities));
+					foreach ($valueEntities as $key => $entity) {
+						if(count($entity) > 0){
+							//var_dump($entity);
+							?>
+							<tr>
+								<td>
+								</td>
+								<td>
+									<a  href='javascript:;' onclick='loadByHash("#<?php echo $key ; ?>.detail.id.<?php echo $entity['id'] ; ?>")' class=''>
+										<?php echo $entity["name"]; ?> 
+									</a>
+								</td>
+								<td> 
+									<?php echo (empty($entity["warnings"])?"": Import::getMessagesWarnings($entity["warnings"])) ; ?>
+								</td>
+							</tr>
+							<?php
+						}
+					}
+					
+				}
+			?>
 
 		</table>
 	</div>
