facd8a07352fe428fbc7c383b4b2ac28cd093249
Clément Damiens <clement.damiens@gmail.com>
2016-03-18 16:47:35 +0100

    fix bug on avatar topMenu

--- views/default/short_info_profil.php
+++ views/default/short_info_profil.php
@@ -136,7 +136,7 @@
     <?php if( isset( Yii::app()->session['userId']) ){ ?>
       <div class="dropdown pull-right hidden-xs">
         <button class="dropdown-toggle menu-name-profil text-dark" data-toggle="dropdown">
-          <img class="img-circle" id="menu-thumb-profil" width="34" height="34" src="<?php echo Yii::app()->createUrl('/'.$this->module->id.'/document/resized/50x50'.$urlPhotoProfil); ?>" alt="image" >
+          <img class="img-circle" id="menu-thumb-profil" width="34" height="34" src="<?php echo $urlPhotoProfil; ?>" alt="image" >
           <?php //echo $me["name"]; ?>
           <span class="caret"></span>
         </button>
