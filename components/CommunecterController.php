262a5612e53b05f8cf8d3ed24897c33c9cd69d49
Sylvain Barbot <sylvain.barbot@gmail.com>
2016-04-20 09:23:58 +0400

    change version number

--- components/CommunecterController.php
+++ components/CommunecterController.php
@@ -5,8 +5,8 @@
  */
 class CommunecterController extends Controller
 {
-  public $version = "v0.0981";
-  public $versionDate = "18/04/2016 16:40";
+  public $version = "v0.0982";
+  public $versionDate = "20/04/2016 09:23";
   public $title = "Communectez";
   public $subTitle = "se connecter à sa commune";
   public $pageTitle = "Communecter, se connecter à sa commune";
