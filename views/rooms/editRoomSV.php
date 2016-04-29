b00e22806ef963c23eb1a64294312d246e0fc063
Kgneo <tristan.goguet@gmail.com>
2016-04-29 17:22:40 +1100

    up form edit room 2 step

--- views/rooms/editRoomSV.php
+++ views/rooms/editRoomSV.php
@@ -6,34 +6,36 @@ $this->renderPartial('../default/panels/toolbar');
     blockquote{border-color: #2BB0C6; cursor: pointer;}
 </style>
 
-<h1 class="homestead center text-dark"><i class="fa fa-caret-down"></i> Quel type d'espace souhaitez-vous créer ?</h1><br/>
-<div class="col-xs-12 col-sm-6 col-md-5 col-md-offset-1 text-dark">
-  <blockquote> 
-<!--     <a class="thumb-info" href="<?php echo $this->module->assetsUrl; ?>/images/screenshots/survey.png" data-title="Proverbs, Culture, Art, Thoughts"  data-lightbox="all">
-    <img id="img-header" class="img-responsive" src="<?php echo $this->module->assetsUrl; ?>/images/screenshots/survey.png"/>
-    </a> -->
-    <i class="fa fa-comments center fa-4x"></i>
-    <br/><br/><a class="btn btn-success" href="javascript:;" onclick="" typeval="discuss"><span class="text-bold"><?php echo Yii::t('rooms', 'Create a discussion', null, Yii::app()->controller->module->id)?> <i class="fa fa-arrow-circle-right"></i></span></a>
-    <br/><br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', "Let's talk about", null, Yii::app()->controller->module->id)?>
-    <!-- <br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', 'Find questions to ask', null, Yii::app()->controller->module->id)?> -->
-    <br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', 'Collective intelligence sometimes starts by talking', null, Yii::app()->controller->module->id)?>
-    <!-- <br/><a class="btn btn-success" href="javascript:;" onclick=""><?php echo Yii::t( "common", 'Do this'); ?></a>  -->
-  </blockquote>
-</div>
+<div id="first-step-create-space">
+  <h1 class="homestead center text-dark"><i class="fa fa-caret-down"></i> Quel type d'espace souhaitez-vous créer ?</h1><br/>
+  <div class="col-xs-12 col-sm-6 col-md-5 col-md-offset-1 text-dark">
+    <blockquote> 
+  <!--     <a class="thumb-info" href="<?php echo $this->module->assetsUrl; ?>/images/screenshots/survey.png" data-title="Proverbs, Culture, Art, Thoughts"  data-lightbox="all">
+      <img id="img-header" class="img-responsive" src="<?php echo $this->module->assetsUrl; ?>/images/screenshots/survey.png"/>
+      </a> -->
+      <i class="fa fa-comments center fa-4x"></i>
+      <br/><br/><a class="btn btn-success" href="javascript:;" onclick="selectRoomType('discuss')"><span class="text-bold"><?php echo Yii::t('rooms', 'Create a discussion', null, Yii::app()->controller->module->id)?> <i class="fa fa-arrow-circle-right"></i></span></a>
+      <br/><br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', "Let's talk about", null, Yii::app()->controller->module->id)?>
+      <!-- <br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', 'Find questions to ask', null, Yii::app()->controller->module->id)?> -->
+      <br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', 'Collective intelligence sometimes starts by talking', null, Yii::app()->controller->module->id)?>
+      <!-- <br/><a class="btn btn-success" href="javascript:;" onclick=""><?php echo Yii::t( "common", 'Do this'); ?></a>  -->
+    </blockquote>
+  </div>
 
-<div class="col-xs-12 col-sm-6 col-md-5 text-dark">
-  <blockquote> 
-    <!-- <a class="thumb-info" href="<?php echo $this->module->assetsUrl; ?>/images/screenshots/discuss.png" data-title="Proverbs, Culture, Art, Thoughts"  data-lightbox="all">
-      <img id="img-header" class="img-responsive" src="<?php echo $this->module->assetsUrl; ?>/images/screenshots/discuss.png"/>
-    </a> -->
-    <i class="fa fa-gavel center fa-4x"></i>
-    <br/><br/><a class="btn btn-success" href="javascript:;" onclick="" typeval="discuss"><span class="text-bold"><?php echo Yii::t('rooms', 'Take decisions', null, Yii::app()->controller->module->id)?></span> <i class="fa fa-arrow-circle-right"></i></a>
-    <!-- <br><?php echo Yii::t('rooms', 'Share Qestions', null, Yii::app()->controller->module->id)?> -->
-    <!-- <br><?php echo Yii::t('rooms', 'Brainstorm', null, Yii::app()->controller->module->id)?> -->
-    <br/><br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', 'Decide Collectivelly', null, Yii::app()->controller->module->id)?>
-    <br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', 'to think, develop, build and decide collaboratively', null, Yii::app()->controller->module->id)?>
-    <!-- <br/><a class="btn btn-success" href="javascript:;" onclick=""><?php echo Yii::t( "common", 'Do this'); ?></a>  -->
-  </blockquote>
+  <div class="col-xs-12 col-sm-6 col-md-5 text-dark">
+    <blockquote> 
+      <!-- <a class="thumb-info" href="<?php echo $this->module->assetsUrl; ?>/images/screenshots/discuss.png" data-title="Proverbs, Culture, Art, Thoughts"  data-lightbox="all">
+        <img id="img-header" class="img-responsive" src="<?php echo $this->module->assetsUrl; ?>/images/screenshots/discuss.png"/>
+      </a> -->
+      <i class="fa fa-gavel center fa-4x"></i>
+      <br/><br/><a class="btn btn-success" href="javascript:;" onclick="selectRoomType('vote')"><span class="text-bold"><?php echo Yii::t('rooms', 'Take decisions', null, Yii::app()->controller->module->id)?></span> <i class="fa fa-arrow-circle-right"></i></a>
+      <!-- <br><?php echo Yii::t('rooms', 'Share Qestions', null, Yii::app()->controller->module->id)?> -->
+      <!-- <br><?php echo Yii::t('rooms', 'Brainstorm', null, Yii::app()->controller->module->id)?> -->
+      <br/><br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', 'Decide Collectivelly', null, Yii::app()->controller->module->id)?>
+      <br><i class="fa fa-caret-right"></i> <?php echo Yii::t('rooms', 'to think, develop, build and decide collaboratively', null, Yii::app()->controller->module->id)?>
+      <!-- <br/><a class="btn btn-success" href="javascript:;" onclick=""><?php echo Yii::t( "common", 'Do this'); ?></a>  -->
+    </blockquote>
+  </div>
 </div>
 
 <div id="editRoomsContainer" class="hidden"></div>
@@ -61,7 +63,7 @@ var roomFormDefinition = {
               },
             "name" :{
               "inputType" : "text",
-              "placeholder" : "Titre de la proposition",
+              "placeholder" : "Nom de l'espace",
               "rules" : {
                 "required" : true
               }
@@ -95,6 +97,21 @@ jQuery(document).ready(function() {
     //openSubView('Add a Room', '/communecter/rooms/editRoom',null,function(){editRoomSV ();});
   });
 });
+
+function selectRoomType(type){
+  $("#roomType").val(type);
+  
+  var msg = "Nouvel espace";
+  if(type=="discuss") msg = "<i class='fa fa-comments'></i> " + msg + " de discussion";
+  if(type=="vote") msg = "<i class='fa fa-gavel'></i> " + msg + " de décision";
+  $("#proposerloiFormLabel").html(msg);
+  $("#proposerloiFormLabel").addClass("text-dark");
+  $("#btn-submit-form").html('<?php echo Yii::t("common", "Submit"); ?> <i class="fa fa-arrow-circle-right"></i>');
+  
+  $("#first-step-create-space").hide(400);
+  $(".roomTypeselect").addClass("hidden");
+  $("#editRoomsContainer").removeClass("hidden");
+}
 
 function editRoomSV (roomObj) { 
   console.warn("--------------- editEntrySV ---------------------");
