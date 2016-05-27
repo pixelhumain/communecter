8d3cc6d0c727011f5fb4faf0a5e00650516458ff
Clément Damiens <clement.damiens@gmail.com>
2016-05-27 13:53:03 +0200

    fix undefined postal code

--- assets/js/news/newsHtml.js
+++ assets/js/news/newsHtml.js
@@ -189,7 +189,8 @@ function buildLineHTML(newsObj,idSession,update)
 		city = "";
 		if(newsObj.type != "activityStream"){
 			if(newsObj.target.type=="citoyens"){
-				postalCode=newsObj.scope.cities[0].postalCode;
+				if(typeof(newsObj.scope.cities[0].postalCode) != "undefined")
+					postalCode=newsObj.scope.cities[0].postalCode;
 				if(typeof(newsObj.scope.cities[0].addressLocality) != "undefined")
 					city=newsObj.scope.cities[0].addressLocality;			
 			}
