f895d535fb1b65e37ee2cc7b99ce23a1a9d16206
Kgneo <tristan.goguet@gmail.com>
2016-06-06 23:28:19 +1100

    creer orga sur saint pierre , sans addresse va se poisitionné sur Bordeaux #936

--- views/event/eventSV.php
+++ views/event/eventSV.php
@@ -763,6 +763,9 @@ HtmlHelper::registerCssAndScriptsFiles( $cssAnsScriptFilesModule ,Yii::app()->th
 			insee=$('#city').val();
 			postalCode=$('#postalCode').val();
 			streetAddress=$('#fullStreet').val();
+			country = $('.form-event #eventCountry').val();
+			country = getFullTextCountry(country);
+			
 			if(streetAddress.length < 2){
 	  			$.ajax({
 					url: baseUrl+"/"+moduleId+"/sig/getlatlngbyinsee",
@@ -796,7 +799,7 @@ HtmlHelper::registerCssAndScriptsFiles( $cssAnsScriptFilesModule ,Yii::app()->th
 			
 	  		} else{
 				
-				var requestPart = streetAddress + ", " + postalCode; // + ", " + $("#addressCountry").val();
+				var requestPart = streetAddress + ", " + country + ", " + postalCode; // + ", " + $("#addressCountry").val();
 				requestPart = transformNominatimUrl(requestPart);
 	
 		  		console.log("requestPart", requestPart);
