56419b85b05e4040aaec46337a2b3be8dc5a79a1
Tibor Katelbach <oceatoon@gmail.com>
2016-06-17 18:47:05 +0400

    better docs

---
+++ views/docs/events.php
@@ -0,0 +1,84 @@
+
+<div class="panel-heading border-light center text-dark partition-white radius-10">
+    <span class=" text-red homestead tpl_title"> DDA : Discuter, Décider, Agir</span>
+    <br/>
+    <span class="tpl_shortDesc">Les salles d'action permettent de créer 3 types d'espaces pour le moment <br/>Discuter pour orienté, comprendre les visions de chacuns puis, Décider pour se mettre d'accord, et enfin Agir pour que ca avance </span>
+</div>
+
+<style type="text/css">
+    ul li {list-style: none}
+    .tpl_title{font-size: 48px;}
+     .panel-title {font-size:25px;}
+    .points{padding-left:10px;}
+</style>
+<div class="col-sm-12 ">
+
+    <div class="panel panel-white ">
+        
+        <div class="panel-body tpl_content">
+           
+        <img src="/ph/assets/a034e4b8/images/docs/events.png" class="col-sm-12 img-responsive ">
+        <div class="col-sm-12" style="margin-top:30px;margin-bottom:30px; " >
+	        <div class="col-sm-4 ">
+		        <div class="panel panel-white user-list ">
+					<div class="panel-heading border-light">
+						<h4 class="panel-title homestead"><i class="fa fa-comments"></i> DISCUTER</h4>
+					</div> 
+					<div class="panel-body">
+						<br/><b>Les Salles de Discussion</b> servent à construire et à partager autour d'une thématique
+						<br/>
+				        <ul class="points">
+				        	<li><i class='fa fa-arrow-right'></i> Discutaire avec des commentaire </li>
+				        	<li><i class='fa fa-arrow-right'></i> surligner du texte et transformer en vote ou action</li>
+				        	<li><i class='fa fa-arrow-right'></i> Bientot : Discussion type Framapad</li>
+				        </ul>
+				    </div>
+				</div>
+			</div>
+
+			<div class="col-sm-4 ">
+		        <div class="panel panel-white user-list ">
+					<div class="panel-heading border-light">
+						<h4 class="panel-title homestead"><i class="fa fa-comments"></i> DECIDER</h4>
+					</div> 
+					<div class="panel-body">
+					<br/><b>Les Salles de Décisions</b> ou Votes permettent de faire des propositions et de les partager avec une communauté pour prendre des décisions
+					<br/>
+			        <ul class="points">
+			        	<li><i class='fa fa-arrow-right'></i> C'est un votation à 5 choix : </li>
+						<li><i class='fa fa-arrow-right'></i> Voter "Pour" : Assez Explicite</li>
+						<li><i class='fa fa-arrow-right'></i> Voter "Amender" : La base est bonne mais il faut encore corriger, améliorer, la rendre meilleure.</li>
+						<li><i class='fa fa-arrow-right'></i> Voter "Blanc" : Je suis ni pour ni contre</li>
+						<li><i class='fa fa-arrow-right'></i> Voter "Incomplet" : il manque des elements pour prendre une réélle décision</li>
+						<li><i class='fa fa-arrow-right'></i> Voter "Contre" : Assez Explicite</li>
+			        </ul>
+			    </div>
+				</div>
+			</div>
+
+			<div class="col-sm-4 ">
+		        <div class="panel panel-white user-list ">
+					<div class="panel-heading border-light">
+						<h4 class="panel-title homestead"><i class="fa fa-comments"></i> AGIR</h4>
+					</div> 
+					<div class="panel-body">
+						<br/><b>Les Salles d'Actions</b> permettent de faire des listes d'actino de choses concrète ç réaliser
+						<br/>
+				        <ul class="points">
+				        	<li><i class='fa fa-arrow-right'></i> une action peut avoir 5 états différents : </li>
+							<li><i class='fa fa-arrow-right'></i> "A Faire" : aucune date de début n'a été assigné</li>
+							<li><i class='fa fa-arrow-right'></i> "En cours" : une date de début, et une personne est assignée</li>
+							<li><i class='fa fa-arrow-right'></i> "En retard": la date de fin est assigné mais dépassé.</li>
+							<li><i class='fa fa-arrow-right'></i> "Terminer" : une tache qui a été cloturé</li>
+							<li><i class='fa fa-arrow-right'></i> "Non Assignée" : une tache qui n'a pas encore de responsable </li>
+							<li><i class='fa fa-arrow-right'></i> Plusieurs personne peuvent etre assignées à une action</li>
+				        </ul>
+				    </div>
+				</div>
+			</div>
+
+	    </div>
+        <br>
+        <div class="col-sm-12 "><a style="display: block;" class="text-extra-large bg-dark pull-left tooltips radius-5 padding-10 homestead" href="javascript:window.history.back();"><i class="fa fa-arrow-left"></i>  Retour </a><a style="display: block;" class="text-extra-large bg-red pull-right tooltips radius-5 padding-10 homestead" href="javascript:loadByHash('#default.view.page.dda.dir.docs');">Events <i class="fa fa-arrow-right"></i> </a></div></div>
+    </div>
+</div>
