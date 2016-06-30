c4482203a75490c27cf740ed65be8be558d3b724
Kgneo <tristan.goguet@gmail.com>
2016-06-30 22:39:59 +1100

    fix bug include css slider player

--- views/docs/docPattern/player.php
+++ views/docs/docPattern/player.php
@@ -1,5 +1,5 @@
 <?php 
-  $cssAnsScriptFilesModule = array('/css/default/docs.css');
+  $cssAnsScriptFilesModule = array('/css/docs/docs.css');
   HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule, $this->module->assetsUrl);
 ?>
 <div id="docCarousel" class="carousel slide" data-ride="carousel">
