1638e383be277bd7f6f3fba4dcfdb304d4e02ae2
Clément Damiens <clement.damiens@gmail.com>
2016-07-12 09:14:43 +0200

    close  #1035 action assignee

--- views/pod/usersList.php
+++ views/pod/usersList.php
@@ -199,10 +199,10 @@
 
 				}
 				echo "<div class='no-padding' style='border-top: 1px solid lightgray;margin-top:10px !important;'>";
-				if (!empty($countStrongLinks)){
+				if (@$countStrongLinks && !empty($countStrongLinks)){
 					if($contentType==Organization::COLLECTION)
 						$strongLinksLabel=Yii::t("common","member");
-					else if ($contentType==Project::COLLECTION)
+					else if ($contentType==Project::COLLECTION || $contentType == ActionRoom::COLLECTION_ACTIONS)
 						$strongLinksLabel=Yii::t("common","contributor");
 					else if ($contentType==Event::COLLECTION)
 						$strongLinksLabel=Yii::t("event","attendee");
@@ -213,7 +213,7 @@
 							"<span class='text-dark'>".ucfirst($strongLinksLabel)."</span>".
 						"</div>";
 					
-				}if ($countLowLinks != 0){
+				}if (@$countLowLinks && $countLowLinks != 0){
 					$style="";		
 					if ($contentType==Event::COLLECTION)
 						$lowLinksLabel = Yii::t("event","guest");
