42240278c39753695893f33050fc051760ed045f
raphael <raphael_riviere@yahoo.fr>
2016-07-20 09:11:04 +0400

    Correction d'orthographe sur les badges

--- messages/fr/badge.php
+++ messages/fr/badge.php
@@ -4,7 +4,7 @@ return array(
 	
 	"crowdfunder" 	=> "Vous avez participer au financement participatif de Communecter",
 	"developper" 	=> "Vous êtes un développeur de Communecter",
-	"opendata" 		=> "Vous contribuer à l&apos;Open Data",
+	"opendata" 		=> "Vous contribuez à l&apos;Open Data",
 	
 );
 
