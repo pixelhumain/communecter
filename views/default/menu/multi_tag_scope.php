59e02d5b68fa46c58dd3948c453b9144fd56f84a
tango <tristan.goguet@gmail.com>
2016-08-08 15:03:28 +1100

    begin multiscope

--- views/default/menu/multi_tag_scope.php
+++ views/default/menu/multi_tag_scope.php
@@ -2,7 +2,8 @@
 
 <style type="text/css">
 
-	.dropdown.open #dropdown-multi-tag{
+	.dropdown.open #dropdown-multi-tag,
+	.dropdown.open #dropdown-multi-scope{
 		display: inline !important;
 	}
 
@@ -27,21 +28,25 @@
 	    -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
 	    box-shadow: 0 6px 12px rgba(0,0,0,.175);
 
-	    width: 500px;
-		min-width: 500px;
-		max-width: 500px;
+	    width: 550px;
+		min-width: 550px;
+		max-width: 550px;
 		margin-top: 4px;
 		border-radius: 0px 0px 4px 4px;
 	}
-
-	#dropdown-multi-tag input.form-control {
+	#dropdown-multi-scope {
+	    left: 0px;
+	}
+	#dropdown-multi-tag input.form-control,
+	#dropdown-multi-scope input.form-control {
 	    text-align: left;
 	    border-radius: 0px !important;
 	    padding: 5px;
 	    height: 34px !important;
 	}
 
-	#dropdown-multi-tag .item-tag-input{
+	#dropdown-multi-tag .item-tag-input,
+	#dropdown-multi-scope .item-scope-input{
 		padding:6px;
 		border-radius:20px;
 		display: inline-block;
@@ -49,16 +54,20 @@
 		margin-top: 3px;
 	}
 	#dropdown-multi-tag .item-tag-input .item-tag-checker:hover,
-	#dropdown-multi-tag .item-tag-input .item-tag-deleter:hover{
+	#dropdown-multi-tag .item-tag-input .item-tag-deleter:hover,
+	#dropdown-multi-scope .item-scope-input .item-scope-checker:hover,
+	#dropdown-multi-scope .item-scope-input .item-scope-deleter:hover{
 		color:#ffa9a9;
 	}
 
-	#dropdown-multi-tag .item-tag-input .item-tag-name{
+	#dropdown-multi-tag .item-tag-input .item-tag-name,
+	#dropdown-multi-scope .item-scope-input .item-scope-name{
 	    padding-left: 5px;
 	    padding-right: 5px;
 	}
 
-	#dropdown-multi-tag .item-tag-input a{
+	#dropdown-multi-tag .item-tag-input a,
+	#dropdown-multi-scope .item-scope-input a{
 	    color:white;
 	}
 
@@ -103,168 +112,11 @@
 	}
 </style>
 
+<?php $this->renderPartial('../default/menu/multi_tag'); ?>
+<?php $this->renderPartial('../default/menu/multi_scope'); ?>
 
 
-<div class="dropdown pull-left">
-  <button class="pull-left tooltips"  data-toggle="dropdown"  id="btn-modal-multi-tag"
-	data-toggle="tooltip" data-placement="bottom" 
-	title="Mes tags favoris">
-	<i class="fa fa-tag" style=""></i>
-  </button>
-  <ul class="dropdown-menu" id="dropdown-multi-tag">
-      <div class="panel-body text-dark padding-bottom-10">
-      		<div class="col-md-12 no-padding">
-	      		<div class="col-md-12">
-	      			<h3 class="no-margin" style="margin-top: 13px ! important;">
-	      				<i class="fa fa-angle-down"></i> <i class="fa fa-tag"></i> Mes <strong>#tags</strong> favoris
-	      			</h3>
-	      			<hr style="margin-top: 10px; margin-bottom: 10px;">
-	      			<div class="input-group margin-bottom-10">
-				      <span class="input-group-btn">
-				        <div class="input-group-addon" type="button">
-				        	<i class="fa fa-plus"></i> <i class="fa fa-tag"></i>
-				        </div>
-				      </span>
-				      <input id="input-add-multi-tag" type="text" class="form-control" placeholder="Ajouter un tag ...">
-				      <span class="input-group-btn">
-				        <button class="btn btn-success btn-add-tag" type="button"><i class="fa fa-check"></i></button>
-				      </span>
-				    </div>
-				    <div class="label label-info label-sm block text-left" id="lbl-info-select-multi-tag"></div>
-	      		</div>
-	      		<div id="multi-tag-list" class="col-md-12 margin-top-15"></div>
-      		</div>
-      		
-      	</div>
-   </ul>
-</div>
-
-
-<div class="dropdown pull-left">
-	<button class="pull-left tooltips"  data-toggle="dropdown"  id="btn-modal-multi-scope"
-		data-toggle="tooltip" data-placement="bottom" 
-		title="Mes lieux favoris">
-		<i class="fa fa-bullseye" style=""></i>
-	</button>
-	<ul class="dropdown-menu" id="dropdown-multi-scope">
-	    <li>
-	      <div class="panel-body text-dark">
-	 	  </div>
-	 	</li>
-  	</ul>
-</div>
-
-
-<script type="text/javascript"> 
-
-
-var myMultiTags = new Array();
-
-jQuery(document).ready(function() {
-	$('ul.dropdown-menu').click(function(){ return false });
-
-	$(".btn-add-tag").click(function(){ console.log("btn-add-tag click()");
-		addTagToMultitag($("#input-add-multi-tag").val())
-	});
-
-	$('#input-add-multi-tag').filter_input({regex:'[a-zA-Z0-9_]'}); 
-
-	loadMultiTags();
-});
-
-function loadMultiTags(){
-	$.each(myMultiTags, function(key, value){
-		showTagInMultitag(key);
-	});
-}
-
-function showTagInMultitag(tagValue){ console.log("showTagInMultitag()", tagValue);
-	var html = "";
-	if(myMultiTags.indexOf(tagValue) < 0){
-		var tag = myMultiTags[tagValue];
-		html = 
-		'<span class="item-tag-input bg-red" data-tag-value="'+tagValue+'">' +
-				'<a  href="javascript:" class="item-tag-checker tooltips"' +
-					'data-toggle="tooltip" data-placement="bottom" ' +
-					'title="Activer/Désactiver" data-tag-value="'+tagValue+'">' +
-					'<i class="fa fa-check-circle"></i>' +
-				'</a>' +
-				'<span class="item-tag-name">#'+tagValue+'</span>' +
-				'<a href="javascript:" class="item-tag-deleter tooltips"' +
-					'data-toggle="tooltip" data-placement="bottom" ' +
-					'title="Supprimer" data-tag-value="'+tagValue+'">' +
-					'<i class="fa fa-times"></i>' +
-			'</a>' +
-		'</span>';
-	}else{
-		html = "";
-		showMsgInfoMultiTag("showTagInMultitag error : ce tag n'existe pas - " + tagValue, "error");
-	}
-	
-	$("#multi-tag-list").append(html);
-	$(".tooltips").tooltip();
-	showMsgInfoMultiTag("Le tag a bien été ajouté", "success");
-	$(".item-tag-checker").off().click(function(){ toogleTagMultitag( $(this).data("tag-value")) });
-	$(".item-tag-deleter").off().click(function(){ deleteTagInMultitag( $(this).data("tag-value")); });
-}
-
-
-function addTagToMultitag(tagValue){  
-	if(tagValue == "") return;
-	if(typeof myMultiTags[tagValue] == "undefined"){
-		console.log("adding", tagValue);
-		myMultiTags[tagValue] = { active: true };
-		showTagInMultitag(tagValue);
-		$("#input-add-multi-tag").val("");
-	}else{
-		showMsgInfoMultiTag("Ce tag est déjà dans votre liste", "info");
-	}
-}
-
-function deleteTagInMultitag(tagValue){ //console.log("deleteTagInMultitag(tagValue)", tagValue);
-	if(typeof myMultiTags[tagValue] != "undefined"){
-		delete myMultiTags[tagValue];
-		$("[data-tag-value="+tagValue+"]").remove();
-	}
-	console.dir(myMultiTags);
-}
-
-function toogleTagMultitag(tagValue){ //console.log("toogleTagMultitag(tagValue)", tagValue);
-	if(typeof myMultiTags[tagValue] != "undefined"){
-		myMultiTags[tagValue].active = !myMultiTags[tagValue].active;
-		
-		if(myMultiTags[tagValue].active){
-			$("[data-tag-value="+tagValue+"] .item-tag-checker i.fa").removeClass("fa-circle-o");
-			$("[data-tag-value="+tagValue+"] .item-tag-checker i.fa").addClass("fa-ckeck-circle");
-		}else{
-			$("[data-tag-value="+tagValue+"] .item-tag-checker i.fa").addClass("fa-circle-o");
-			$("[data-tag-value="+tagValue+"] .item-tag-checker i.fa").removeClass("fa-ckeck-circle");
-		}
-	}else{
-		showMsgInfoMultiTag("Ce tag n'existe pas", "danger");
-	}
-}
-
-function getMultiTagList(){ return myMultiTags; }
-
-var timerMsgMultitag;
-function showMsgInfoMultiTag(msg, type){
-	if(type == "success") msg = "<i class='fa fa-check'></i> " + msg;
-	if(type == "danger") msg = "<i class='fa fa-times'></i> " + msg;
-	if(type == "info") msg = "<i class='fa fa-info-circle'></i> " + msg;
-	
-	var id = "#lbl-info-select-multi-tag";
-	$(id).html(msg);
-	if(type == "success") $(id).addClass("label-success"); else $(id).removeClass("label-success");
-	if(type == "danger") $(id).addClass("label-danger"); else $(id).removeClass("label-danger");
-	if(type == "info") $(id).addClass("label-info"); else $(id).removeClass("label-info");
-
-	$(id).off().hide();
-	$(id).show(200);
-
-	if(typeof timerMsgMultitag != "undefined") clearTimeout(timerMsgMultitag);
-	timerMsgMultitag = setTimeout(function(){ $(id).off().hide(500)}, 3000);
-}
+<script>
 /*function openCommonModal(hash){ console.log("search for modal key :", hash);
 	var urls = {
 		"organization.addorganizationform": { 
