d697b2d2dbdb1fbfb74b57b12f136361da961458
Tibor Katelbach <oceatoon@gmail.com>
2016-08-13 20:17:25 +0400

    gabarit live page

--- views/default/live.php
+++ views/default/live.php
@@ -1,36 +1,197 @@
-<div class="col-xs-12 infoPanel dataPanel">
+<style type="text/css">
+	.border-dark{
+		border:1px solid #ccc;
+	}
+</style>
+<div class="col-xs-12 ">
 		<div class="row">
-			<div class="col-sm-12 col-xs-12 col-md-8">
+			<div class="col-sm-12 col-xs-12 col-md-6">
 	    		
 
-	    		<div class="col-md-12 col-sm-12 col-xs-12 no-padding pull-left">
-					<div class="row padding-15">
-						<hr>
-						<a href='javascript:loadByHash("#rooms.index.type.organizations.id.<?php //echo (String) $organization["_id"]; ?>")'>
-				        	<h1 class="text-azure text-left homestead no-margin">
-				        		<i class='fa fa-angle-down'></i> <i class='fa fa-connectdevelop'></i> Espace coopératif <i class='fa fa-sign-in'></i> <span class="text-small helvetica">(activité récente)</span>
-				        	</h1>
-				        </a>
+	    		<div class="col-md-12 col-sm-12 col-xs-12 margin-bottom-30 border-dark padding-5">
+					<div class=" border-bottom">
+						27/07/50 - Luke Rony  > Hacking Debout
 				    </div>
-					<?php 
-						$list = ActionRoom::getAllRoomsActivityByTypeId(Person::COLLECTION, Yii::app()->session['userId']);	
-						$this->renderPartial('../pod/activityList2',array(    
-		   					//"parent" => $organization, 
-		                    "parentId" => Yii::app()->session['userId'], 
-		                    "parentType" => Person::COLLECTION, 
-		                    "title" => "Activité Coop",
-                        	"list" => @$list, 
-		                    "renderPartial" => true
-		                    ));
-					?>	
+				    <div class=" border-bottom padding-bottom-10">
+				    	<img src="http://placehold.it/450x200" class="img-responsive">
+						<div class="text-bold text-extra-large">Retour sur le jardin de Belleville</div>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				    </div>
+				    <div class=" padding-5">
+				    	<div class="pull-left">
+							<i class="fa fa-thumbs-up"></i> 540 
+							<i class="fa fa-comment"></i> 54 
+						</div>
+						<div class="pull-right">
+							<i class="fa fa-hand-grab-o"></i> 
+							<i class="fa fa-share-alt"></i>
+						</div>
+				    </div>
 				</div>
+
+				<div class="col-md-12 col-sm-12 col-xs-12  border-dark padding-5">
+					<div class=" border-bottom">
+						27/07/50 - Luke Rony  > Hacking Debout
+				    </div>
+				    <div class=" border-bottom padding-bottom-10">
+				    	<img src="http://placehold.it/450x200" class="img-responsive">
+						<div class="text-bold text-extra-large">Retour sur le jardin de Belleville</div>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				    </div>
+				    <div class="  padding-5">
+				    	<div class="pull-left">
+							<i class="fa fa-thumbs-up"></i> 540 
+							<i class="fa fa-comment"></i> 54 
+						</div>
+						<div class="pull-right">
+							<i class="fa fa-hand-grab-o"></i> 
+							<i class="fa fa-share-alt"></i>
+						</div>
+				    </div>
+				</div>
+
+				<div class="col-md-12 col-sm-12 col-xs-12  border-dark padding-5">
+					<div class=" border-bottom">
+						27/07/50 - Luke Rony  > Hacking Debout
+				    </div>
+				    <div class=" border-bottom padding-bottom-10">
+				    	<img src="http://placehold.it/450x200" class="img-responsive">
+						<div class="text-bold text-extra-large">Retour sur le jardin de Belleville</div>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				    </div>
+				    <div class=" padding-5">
+				    	<div class="pull-left">
+							<i class="fa fa-thumbs-up"></i> 540 
+							<i class="fa fa-comment"></i> 54 
+						</div>
+						<div class="pull-right">
+							<i class="fa fa-hand-grab-o"></i> 
+							<i class="fa fa-share-alt"></i>
+						</div>
+				    </div>
+				</div>
+
+				<div class="col-md-12 col-sm-12 col-xs-12  border-dark padding-5">
+					<div class=" border-bottom">
+						27/07/50 - Luke Rony  > Hacking Debout
+				    </div>
+				    <div class=" border-bottom padding-bottom-10">
+				    	<img src="http://placehold.it/450x200" class="img-responsive">
+						<div class="text-bold text-extra-large">Retour sur le jardin de Belleville</div>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				    </div>
+				    <div class="  padding-5">
+				    	<div class="pull-left">
+							<i class="fa fa-thumbs-up"></i> 540 
+							<i class="fa fa-comment"></i> 54 
+						</div>
+						<div class="pull-right">
+							<i class="fa fa-hand-grab-o"></i> 
+							<i class="fa fa-share-alt"></i>
+						</div>
+				    </div>
+				</div>
+
 	    	</div>
 
 	    	
-	    	<div class="col-md-4 no-padding">
-		    	<div class="col-md-12 col-xs-12">
-					
-		    	</div>
+	    	<div class="col-sm-12 col-xs-12 col-md-6 ">
+
+	    		<div class="col-sm-12 col-xs-12 col-md-6 ">
+			    	
+			    	<div class="border-dark margin-bottom-30">
+						
+					    <div class=" ">
+							<img src="http://placehold.it/250x100" class="img-responsive">
+					    </div>
+					    <div class="padding-5 ">
+							27/07/50 - Luke Rony 
+							<br/><div class="text-right">Hacking Debout</div>
+					    </div>
+					</div>
+
+					<div class="border-dark margin-bottom-30">
+						
+					    <div class=" ">
+							<img src="http://placehold.it/250x100" class="img-responsive">
+					    </div>
+					    <div class="padding-5 ">
+							27/07/50 - Luke Rony 
+							<br/><div class="text-right">Hacking Debout</div>
+					    </div>
+					</div>
+
+					<div class="border-dark margin-bottom-30">
+						
+					    <div class=" ">
+							<img src="http://placehold.it/250x100" class="img-responsive">
+					    </div>
+					    <div class="padding-5 ">
+							27/07/50 - Luke Rony 
+							<br/><div class="text-right">Hacking Debout</div>
+					    </div>
+					</div>
+				</div>
+
+
+
+
+				<div class="col-sm-12 col-xs-12 col-md-6 ">
+			    	<div class="border-dark margin-bottom-30">
+						
+					    <div class=" ">
+							<img src="http://placehold.it/250x100" class="img-responsive">
+					    </div>
+					    <div class="padding-5 ">
+							27/07/50 - Luke Rony 
+							<br/><div class="text-right">Hacking Debout</div>
+					    </div>
+					</div>
+
+					<div class="border-dark margin-bottom-30">
+						
+					    <div class=" ">
+							<img src="http://placehold.it/250x100" class="img-responsive">
+					    </div>
+					    <div class="padding-5  ">
+							27/07/50 - Luke Rony 
+							<br/><div class="text-right">Hacking Debout</div>
+					    </div>
+					</div>
+
+					<div class="border-dark margin-bottom-30">
+						
+					    <div class=" ">
+							<img src="http://placehold.it/250x100" class="img-responsive">
+					    </div>
+					    <div class="padding-5 ">
+							27/07/50 - Luke Rony 
+							<br/><div class="text-right">Hacking Debout</div>
+					    </div>
+					</div>
+				</div>
+
+
 			</div>
 
 	    </div>
