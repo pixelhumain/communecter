1da5fb16360859526005f1920d42dc6f4a01b850
tango <tristan.goguet@gmail.com>
2016-09-02 03:30:00 +1100

    attempt fix bug event undefined index type

--- views/pod/eventsList.php
+++ views/pod/eventsList.php
@@ -48,10 +48,10 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesTheme);
 								<?php  
 								$url = '#event.detail.id.'.$e["_id"];
 
-								if(@$organiserImgs && @$e["links"]["organizer"] && isset($e["links"]["organizer"][$id]['type'])){
+								if(@$organiserImgs && @$e["links"]["organizer"]){
 
 									$id = array_keys($e["links"]["organizer"])[0];
-									$o = Element::getInfos( $e["links"]["organizer"][$id]['type'], $id);
+									$o = Element::getInfos( @$e["links"]["organizer"][$id]['type'], $id);
 									if ($o["type"]==Person::COLLECTION){
 										$icon='<img height="35" width="35" class="tooltips" data-placement="right" src="'.$this->module->assetsUrl.'/images/news/profile_default_l.png" data-placement="right" data-original-title="'.$o['name'].'">';
 										$refIcon="fa-user";
