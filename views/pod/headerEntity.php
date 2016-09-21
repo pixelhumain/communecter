3716bee00b2311606d13e483c5c5f351ab101079
Clément Damiens <clement.damiens@gmail.com>
2016-09-21 18:08:10 +0200

    fix bug on chart

--- views/pod/headerEntity.php
+++ views/pod/headerEntity.php
@@ -553,41 +553,119 @@ var contextMap = [];
 var element = <?php echo isset($entity) ? json_encode($entity) : "''"; ?>;
 
 jQuery(document).ready(function() {
-	//setTimeout(function () {
-		// Cette fonction s'exécutera dans 5 seconde (1000 millisecondes)
-		$.ajaxSetup({ cache: true});
-		$.ajax({
-			url: baseUrl+"/"+moduleId+"/element/getalllinks/type/<?php echo $type ;?>/id/<?php echo (string)$entity["_id"] ?>",
-			type: 'POST',
-			data:{ "links" : elementLinks },
-			//async:false,
-			cache: true,
-			dataType: "json",
-			//complete: function () {},
-			success: function (obj){
-				console.log("conntext/////");
-				console.log(obj);
-				Sig.restartMap();
-				Sig.showMapElements(Sig.map, obj);	
-				contextMap = obj;	
-				$(".communityBtn").removeClass("hide");
-			},
-			error: function (error) {
-				console.log("error findGeoposByInsee");
-				callbackFindByInseeError(error);	
-				$("#iconeChargement").hide();	
-			}
-		});	
-	//}, 1000);
+	$.ajaxSetup({ cache: true});
+	$.ajax({
+		url: baseUrl+"/"+moduleId+"/element/getalllinks/type/<?php echo $type ;?>/id/<?php echo (string)$entity["_id"] ?>",
+		type: 'POST',
+		data:{ "links" : elementLinks },
+		//async:false,
+		cache: true,
+		dataType: "json",
+		//complete: function () {},
+		success: function (obj){
+			console.log("conntext/////");
+			console.log(obj);
+			Sig.restartMap();
+			Sig.showMapElements(Sig.map, obj);	
+			contextMap = obj;	
+			$(".communityBtn").removeClass("hide");
+		},
+		error: function (error) {
+			console.log("error findGeoposByInsee");
+			callbackFindByInseeError(error);	
+			$("#iconeChargement").hide();	
+		}
+	});	
 
-	/*bindHeaderElement();
-	activateEditableContextHeader();
-	manageModeContextHeader();*/
-	/*$(".btn-menu-element").click(function(){
-		$(".btn-menu-element").removeClass("active");
-		$(this).addClass("active");
-	});*/
-
+	// Init Chart
+	if(contextType == "<?php echo Project::COLLECTION ?>"){
+		Chart.defaults.global = {
+			// Boolean - Whether to animate the chart
+			animation: true,
+		    // Number - Number of animation steps
+		    animationSteps: 60,
+		    // String - Animation easing effect
+		    animationEasing: "easeOutQuart",
+		    // Boolean - If we should show the scale at all
+		    showScale: true,
+		    // Boolean - If we want to override with a hard coded scale
+		    scaleOverride: false,
+		    // ** Required if scaleOverride is true **
+		    // Number - The number of steps in a hard coded scale
+		    scaleSteps: null,
+		    // Number - The value jump in the hard coded scale
+		    scaleStepWidth: null,
+		    // Number - The scale starting value
+		    scaleStartValue: null,
+		    // String - Colour of the scale line
+		    scaleLineColor: "rgba(0,0,0,.1)",
+		    // Number - Pixel width of the scale line
+		    scaleLineWidth: 1,
+		    // Boolean - Whether to show labels on the scale
+		    scaleShowLabels: true,
+		    // Interpolated JS string - can access value
+		    scaleLabel: "<%=value%>",
+		    // Boolean - Whether the scale should stick to integers, not floats even if drawing space is there
+		    scaleIntegersOnly: true,
+		    // Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+		    scaleBeginAtZero: false,
+		    // String - Scale label font declaration for the scale label
+		    scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+		    // Number - Scale label font size in pixels
+		    scaleFontSize: 18,
+		    // String - Scale label font weight style
+		    scaleFontStyle: "normal",
+		    // String - Scale label font colour
+		    scaleFontColor: "#666",
+		    // Boolean - whether or not the chart should be responsive and resize when the browser does.
+		    responsive: true,
+		    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+		    maintainAspectRatio: true,
+		    // Boolean - Determines whether to draw tooltips on the canvas or not
+		    showTooltips: true,
+		    // Function - Determines whether to execute the customTooltips function instead of drawing the built in tooltips (See [Advanced - External Tooltips](#advanced-usage-custom-tooltips))
+		    customTooltips: false,
+		    // Array - Array of string names to attach tooltip events
+		    tooltipEvents: ["mousemove", "touchstart", "touchmove"],
+		    // String - Tooltip background colour
+		    tooltipFillColor: "rgba(0,0,0,0.8)",
+		    // String - Tooltip label font declaration for the scale label
+		    tooltipFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+		    // Number - Tooltip label font size in pixels
+		    tooltipFontSize: 14,
+		    // String - Tooltip font weight style
+		    tooltipFontStyle: "normal",
+		    // String - Tooltip label font colour
+		    tooltipFontColor: "#fff",
+		    // String - Tooltip title font declaration for the scale label
+		    tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+		    // Number - Tooltip title font size in pixels
+		    tooltipTitleFontSize: 14,
+		    // String - Tooltip title font weight style
+		    tooltipTitleFontStyle: "bold",
+		    // String - Tooltip title font colour
+		    tooltipTitleFontColor: "#fff",
+		    // Number - pixel width of padding around tooltip text
+		    tooltipYPadding: 6,
+		    // Number - pixel width of padding around tooltip text
+		    tooltipXPadding: 6,
+		    // Number - Size of the caret on the tooltip
+		    tooltipCaretSize: 8,
+		    // Number - Pixel radius of the tooltip border
+		    tooltipCornerRadius: 6,
+		    // Number - Pixel offset from point x to tooltip edge
+		    tooltipXOffset: 10,
+		    // String - Template string for single tooltips
+		    tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+		    // String - Template string for multiple tooltips
+		    multiTooltipTemplate: "<%= value %>",
+		    // Function - Will fire on animation progression.
+		    onAnimationProgress: function(){},
+		    // Function - Will fire on animation completion.
+		    onAnimationComplete: function(){}
+		}
+	}
+	
 	if(element.address.addressLocality == ""){
 		$(".cobtnHeader,.whycobtnHeader").removeClass("hidden");
 		$("#addressHeader").addClass("hidden");
@@ -731,7 +809,6 @@ function showElementPad(type, id){
 	ajaxPost('#pad-element-container',baseUrl+'/'+moduleId+'/'+url+"renderPartial=true", 
 			data,
 			function(){ 
-				//$(".btn-menu-element").removeClass("active");
 				history.pushState(null, "New Title", "#" + hash);
 				$("#pad-element-container").show(200);
 				bindLBHLinks();
