4d32e7fd18d598debbc2a435a04209cfa66dd8df
Sylvain Barbot <sylvain.barbot@gmail.com>
2016-09-26 11:43:00 +0400

    change tooltip

--- views/default/menu/menuTop.php
+++ views/default/menu/menuTop.php
@@ -145,7 +145,7 @@
 	<?php if(isset(Yii::app()->session['userId'])){ ?>
 	<button class="btn-menu btn-menu-top bg-white text-dark tooltips pull-right" id="btn-show-floopdrawer" 
 			onclick="showFloopDrawer(true)"
-			data-toggle="tooltip" data-placement="bottom" title="Communautés" alt="Afficher mes contacts">
+			data-toggle="tooltip" data-placement="bottom" title="Communautés" alt="Afficher mon réseau">
 			<i class="fa fa-group"></i>
 	</button>
 	<?php } ?>
