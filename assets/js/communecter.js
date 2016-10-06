1cf1ddcdb47623e912a974274cdae584434d263c
Tibor Katelbach <oceatoon@gmail.com>
2016-10-06 11:31:54 +0400

    added shortcuts

--- assets/js/communecter.js
+++ assets/js/communecter.js
@@ -3,6 +3,7 @@ var countPoll = 0;
 $(document).ready(function() { 
 	initSequence();
 	setTimeout( function () { checkPoll() }, 10000);
+	document.onkeydown = checkKeycode;
 });
 
 var prevStep = 0;
@@ -2546,6 +2547,51 @@ function shadowOnHeader() {
 	var y = $(".my-main-container").scrollTop(); 
     if (y > 0) {  $('.main-top-menu').addClass('shadow'); }
     else { $('.main-top-menu').removeClass('shadow'); }
+}
+
+/* ************************************
+Keyboard Shortcuts
+*************************************** */
+var keycodeObj = {"backspace":8,"tab":9,"enter":13,"shift":16,"ctrl":17,"alt":18,"pause/break":19,"capslock":20,"escape":27,"pageup":33,"pagedown":34,"end":35,
+"home":36,"left":37,"up":38,"right":39,"down":40,"insert":45,"delete":46,"0":48,"1":49,"2":50,"3":51,"4":52,"5":53,"6":54,"7":55,"8":56,"9":57,
+"a":65,"b":66,"c":67,"d":68,"e":69,"f":70,"g":71,"h":72,"i":73,"j":74,"k":75,"l":76,"m":77,"n":78,"o":79,"p":80,"q":81,"r":82,"s":83,"t":84,"u":85,"v":86,"w":87,
+"x":88,"y":89,"z":90,"left window key":91,"right window key":92,"select key":93,"numpad 0":96,"numpad 1":97,"numpad 2":98,"numpad 3":99,"numpad 4":100,"numpad 5":101,
+"numpad 6":102,"numpad 7":103,"numpad 8":104,"numpad 9":105,"multiply":106,"add":107,"subtract":109,"decimal point":110,"divide":111,"f1":112,"f2":113,"f3":114,
+"f4":115,"f5":116,"f6":117,"f7":118,"f8":119,"f9":120,"f10":121,"f11":122,"f12":123,"num lock":144,"scroll lock":145,"semi-colon":186,"equal sign":187,
+"comma":188,"dash":189,"period":190,"forward slash":191,"grave accent":192,"open bracket":219,"back slash":220,"close braket":221,"single quote":222};		
+
+function keyMap(str){
+   var ValidChars = "0123456789";
+   var IsNumber=true;
+   for (i = 0; i < str.length && IsNumber == true; i++) { 
+      if (ValidChars.indexOf(str.charAt(i)) == -1) {
+         IsNumber = false;}
+   }
+   
+   if(!IsNumber){str = keycodeObj[str];} 
+  	
+	switch(str){
+		case keycodeObj["e"] : openForm('event'); break;
+		case keycodeObj["o"] : openForm('organization'); break;
+		case keycodeObj["p"] : openForm('project'); break;
+		case keycodeObj["i"] : openForm('person'); break;
+		case keycodeObj["a"] : openForm('action'); break;
+		case keycodeObj["v"] : openForm('entry'); break;
+	}
+}
+
+function checkKeycode(e) {
+	var keycode;
+	if (window.event) {keycode = window.event.keyCode;e=event;}
+	else if (e){ keycode = e.which;}
+	//alert("keycode: " + keycode);
+	if(e.ctrlKey && $.inArray(keycode, [keycodeObj["e"] ,keycodeObj["o"] ,keycodeObj["p"] ,keycodeObj["i"] ,keycodeObj["a"] ,keycodeObj["v"] ] ) ){
+		alert("combination");//shiftKey ctrlKey altKey
+		keyMap(keycode);
+	}
+	/*else{
+		keyMap(keycode);
+	}*/
 }
 
 /*
