7f4727dc6a9112796872726c1dc30795b734a339
Sylvain Barbot <sylvain.barbot@gmail.com>
2016-10-12 10:17:26 +0400

    hide result when looking for postal code with 4c

--- assets/js/postalCode.js
+++ assets/js/postalCode.js
@@ -29,10 +29,10 @@ $(function(){
       var timeout;
       searchValue = $('.editableform #postalCode').val();
       if(searchValue.length == 5 || searchValue.length == 4) {
-        if (! $("#cityDiv").is(":visible")) {
+        //if (! $("#cityDiv").is(":visible")) {
           $("#city").empty();
           runShowCity();
-        }
+        //}
       } else {
         $("#cityDiv").slideUp("medium");
         $("#city").empty();
@@ -43,6 +43,7 @@ $(function(){
       var citiesByPostalCode = getCitiesByPostalCode(searchValue);
       
       if (citiesByPostalCode.length == 0 ){
+        $("#cityDiv").hide();
         $("#postalCodeError").show();
         return;
       } else {
