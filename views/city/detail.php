6d03ec4bde9f486719c6b9f1f5c87abca2202ffb
Kgneo <tristan.goguet@gmail.com>
2016-11-09 00:02:57 +1100

    color citizenCouncil choice

--- views/city/detail.php
+++ views/city/detail.php
@@ -227,8 +227,8 @@ $this->renderPartial('../default/panels/toolbar');
                    <?php }else{?>
                       <div class="col-xs-6 col-sm-3 col-md-3 center text-red no-padding" style="margin-bottom:10px; font-size:17px; font-weight: 300;">
                           <label class="btn btn-discover bg-red"><i class="fa fa-group"></i></label>
-                          <br/><span class='text-red discover-subtitle'><strong>Conseil citoyen</strong><br/>
-                          <select id="selectRoom">
+                          <br/><span class='text-red discover-subtitle'>Conseil citoyen<br/>
+                          <select id="selectRoom" class="text-red">
                               <option value="">Choisir</option>
                               <?php 
                                 foreach ($city["postalCodes"] as $key => $value) {
