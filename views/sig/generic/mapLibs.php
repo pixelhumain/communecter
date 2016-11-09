37c6e7e1457c83511dc1e8dc599a1aef2b2e818a
Kgneo <tristan.goguet@gmail.com>
2016-11-09 14:49:26 +1100

    up mapbox activation & lib

--- views/sig/generic/mapLibs.php
+++ views/sig/generic/mapLibs.php
@@ -11,45 +11,50 @@
 	);
 	HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule, Yii::app()->theme->baseUrl);
 
-		$cssAndScriptFiles = array(
-			
-			//'/js/sig/leaflet/leaflet.js',
-			'/js/sig/leaflet/leaflet.draw-src.js',
-			'/js/sig/leaflet/leaflet.draw.js',
-			'/js/sig/leaflet/leaflet.markercluster-src.js',
-			'/js/sig/leaflet/leaflet.awesome-markers.min.js',
-			'/js/sig/leaflet/leaflet.bouncemarker.js',
+	if(Yii::app()->params["forceMapboxActive"]==false && 
+		Yii::app()->params["mapboxActive"]==false) 
+		$leaflet = array('/js/sig/leaflet/leaflet.js');
 
-			'/js/sig/map.js' , 
-			'/js/sig/map_initializer.js' ,
-			'/js/sig/map_panel.js' ,
-			'/js/sig/map_popupContent.js' ,
-			'/js/sig/map_rightList.js' ,
-			'/js/sig/map_findPlace.js' ,
-			'/js/sig/map_findPlace.js' ,
-			'/js/sig/map_charts.js',
-		);
+	$cssAndScriptFiles = array(
+		//'/js/sig/leaflet/leaflet.js',
+		'/js/sig/leaflet/leaflet.draw-src.js',
+		'/js/sig/leaflet/leaflet.draw.js',
+		'/js/sig/leaflet/leaflet.markercluster-src.js',
+		'/js/sig/leaflet/leaflet.awesome-markers.min.js',
+		'/js/sig/leaflet/leaflet.bouncemarker.js',
 
-		if(isset($sigParams) && isset($sigParams["useChartsMarkers"])){
-			if($sigParams["useChartsMarkers"] == true){ 
+		'/js/sig/map.js' , 
+		'/js/sig/map_initializer.js' ,
+		'/js/sig/map_panel.js' ,
+		'/js/sig/map_popupContent.js' ,
+		'/js/sig/map_rightList.js' ,
+		'/js/sig/map_findPlace.js' ,
+		'/js/sig/map_findPlace.js' ,
+		'/js/sig/map_charts.js',
+	);
 
-				$cssAndScriptFilesCharts = array(
-					'/css/sig/dvf.css',
-					'/js/sig/dvf/leaflet.dvf.chartmarkers.js',
-					'/js/sig/dvf/leaflet.dvf.controls.js',
-					'/js/sig/dvf/leaflet.dvf.datalayer.js',
-					'/js/sig/dvf/leaflet.dvf.linearfunctions.js',
-					'/js/sig/dvf/leaflet.dvf.lines.js',
-					'/js/sig/dvf/leaflet.dvf.markers.js',
-					'/js/sig/dvf/leaflet.dvf.palettes.js',
-					'/js/sig/dvf/leaflet.dvf.regularpolygon.js',
-					'/js/sig/dvf/leaflet.dvf.utils.js'
-				);
+	if(@$leaflet) array_merge($leaflet, $cssAndScriptFiles);
 
-				$cssAndScriptFiles = array_merge($cssAndScriptFiles, $cssAndScriptFilesCharts);
-			}
+	if(isset($sigParams) && isset($sigParams["useChartsMarkers"])){
+		if($sigParams["useChartsMarkers"] == true){ 
+
+			$cssAndScriptFilesCharts = array(
+				'/css/sig/dvf.css',
+				'/js/sig/dvf/leaflet.dvf.chartmarkers.js',
+				'/js/sig/dvf/leaflet.dvf.controls.js',
+				'/js/sig/dvf/leaflet.dvf.datalayer.js',
+				'/js/sig/dvf/leaflet.dvf.linearfunctions.js',
+				'/js/sig/dvf/leaflet.dvf.lines.js',
+				'/js/sig/dvf/leaflet.dvf.markers.js',
+				'/js/sig/dvf/leaflet.dvf.palettes.js',
+				'/js/sig/dvf/leaflet.dvf.regularpolygon.js',
+				'/js/sig/dvf/leaflet.dvf.utils.js'
+			);
+
+			$cssAndScriptFiles = array_merge($cssAndScriptFiles, $cssAndScriptFilesCharts);
 		}
+	}
 
-		HtmlHelper::registerCssAndScriptsFiles($cssAndScriptFiles, $this->module->assetsUrl);
+	HtmlHelper::registerCssAndScriptsFiles($cssAndScriptFiles, $this->module->assetsUrl);
 ?>
 
