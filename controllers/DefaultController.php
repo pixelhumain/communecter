b017c535579ece358d50279cc160348d2d19aab0
Tibor Katelbach <oceatoon@gmail.com>
2017-01-11 13:45:12 +0400

    repaired menuSmall.open

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -25,7 +25,8 @@ class DefaultController extends CommunecterController {
 
 	public function actionIndex() 
 	{
-    	Yii::app()->theme = "ph-dori"; //"ph-dori";      
+    	//Yii::app()->theme = "ph-dori"; //"ph-dori";      
+      Yii::app()->theme = "notragora";
 	    if(@$_GET["network"]){
 	      Yii::app()->theme = "network";
 	      Yii::app()->params['networkParams'] = $_GET["network"];
