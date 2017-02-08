99257388784726d9a91ad6aabe78d23687ad907b
Sylvain Barbot <sylvain.barbot@gmail.com>
2017-02-08 17:33:16 +0400

    move network configuration in the Communecter module

--- CommunecterModule.php
+++ CommunecterModule.php
@@ -7,8 +7,8 @@
  *
 */
 
-class CommunecterModule extends CWebModule
-{
+class CommunecterModule extends CWebModule {
+
 	public function init()
 	{
 		// this method is called when the module is being created
@@ -21,8 +21,11 @@ class CommunecterModule extends CWebModule
 		));
 		
 		Yii::app()->homeUrl = Yii::app()->createUrl($this->id);
-		Yii::app()->theme = $this->getTheme();
-
+		
+		//Apply theme
+		$themeName = $this->getTheme();
+		Yii::app()->theme = $themeName;
+		
 		Yii::app()->language = (isset(Yii::app()->session["lang"])) ? Yii::app()->session["lang"] : 'fr';
 		
 		// import the module-level models and components
@@ -81,8 +84,42 @@ class CommunecterModule extends CWebModule
 
 		if(@$_GET["network"]) {
             $theme = "network";
-            Yii::app()->params['networkParams'] = $_GET["network"];
+            //Yii::app()->params['networkParams'] = $_GET["network"];
         }
 		return $theme;
+	}
+
+	/**
+	 * Récupère le fichier de configuration du network et retourne en tableau json
+	 * Le fichier de conf peut être en local sur le serveur ou accessible depuis une URL
+	 * TODO : Vérifie le bon formatage du fichier
+	 * @return json_decode array
+	 */
+	public function getNetworkParams() {
+		$configPath = "";
+		if(@$_GET["network"]) {
+            Yii::app()->params['networkParams'] = $_GET["network"];
+        }
+        
+        if (empty(Yii::app()->params['networkParams'])) {
+			$configPath = "default";
+		} else {
+			$configPath = Yii::app()->params['networkParams'];
+		}
+		
+		if ( stripos($configPath, "http") != false || stripos($configPath, "https") != false ) {
+			error_log("chargement du fichier de config en local");
+			$configPath =  Yii::app()->theme->basePath . '/views/layouts/params/'.$configPath.".json";
+		}
+
+		try {
+			$json = file_get_contents($configPath, null, null, 0, 10000);
+			if ($json === false) 
+				throw new CHttpException(404, "Impossible to find the network configuration file.");
+		} catch (Exception $e) {
+    		throw new CHttpException(404, "Error Reading the network configuration file.");
+		}
+
+		return json_decode($json, true);
 	}
 }
