e344b80cc45a4837f4a988fbb2a17b1fc74158c8
Sylvain Barbot <sylvain.barbot@gmail.com>
2017-02-08 18:07:09 +0400

    Use Network Model to load conf

--- CommunecterModule.php
+++ CommunecterModule.php
@@ -26,6 +26,11 @@ class CommunecterModule extends CWebModule {
 		$themeName = $this->getTheme();
 		Yii::app()->theme = $themeName;
 		
+		//Retrieve network params in the url
+		if(@$_GET["network"]) {
+            Yii::app()->params['networkParams'] = $_GET["network"];
+        }
+
 		Yii::app()->language = (isset(Yii::app()->session["lang"])) ? Yii::app()->session["lang"] : 'fr';
 		
 		// import the module-level models and components
@@ -87,39 +92,5 @@ class CommunecterModule extends CWebModule {
             //Yii::app()->params['networkParams'] = $_GET["network"];
         }
 		return $theme;
-	}
-
-	/**
-	 * Récupère le fichier de configuration du network et retourne en tableau json
-	 * Le fichier de conf peut être en local sur le serveur ou accessible depuis une URL
-	 * TODO : Vérifie le bon formatage du fichier
-	 * @return json_decode array
-	 */
-	public function getNetworkParams() {
-		$configPath = "";
-		if(@$_GET["network"]) {
-            Yii::app()->params['networkParams'] = $_GET["network"];
-        }
-        
-        if (empty(Yii::app()->params['networkParams'])) {
-			$configPath = "default";
-		} else {
-			$configPath = Yii::app()->params['networkParams'];
-		}
-		
-		if ( stripos($configPath, "http") != false || stripos($configPath, "https") != false ) {
-			error_log("chargement du fichier de config en local");
-			$configPath =  Yii::app()->theme->basePath . '/views/layouts/params/'.$configPath.".json";
-		}
-
-		try {
-			$json = file_get_contents($configPath, null, null, 0, 10000);
-			if ($json === false) 
-				throw new CHttpException(404, "Impossible to find the network configuration file.");
-		} catch (Exception $e) {
-    		throw new CHttpException(404, "Error Reading the network configuration file.");
-		}
-
-		return json_decode($json, true);
 	}
 }
