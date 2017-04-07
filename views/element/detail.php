f3424b379d2edcee26ef7b375b4fae0b1417fe08
Sylvain Barbot <sylvain.barbot@gmail.com>
2017-04-07 16:05:54 +0400

    remove POI on person

--- views/element/detail.php
+++ views/element/detail.php
@@ -383,7 +383,7 @@ if($('#breadcum').length)
 			</div>
 			<?php }
 			} ?>
-			<?php if($type==Project::COLLECTION || $type==Organization::COLLECTION || $type==Event::COLLECTION  || $type==Person::COLLECTION ){ 
+			<?php if($type==Project::COLLECTION || $type==Organization::COLLECTION || $type==Event::COLLECTION){ 
 				if(!@$front || (@$front && $front["poi"]))					{ 
 			?> 
 			<div class="col-xs-12">
