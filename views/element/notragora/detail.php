43d186a05b813f13bf0f8dec8e24359d5ac2c9e3
Oceatoon <oceatoon@gmail.com>
2017-04-10 13:01:46 +0400

    editing as creator

--- views/element/notragora/detail.php
+++ views/element/notragora/detail.php
@@ -210,7 +210,7 @@
 		<div class="element-name text-dark">
 			<?php echo @$element["name"]; ?>
 			<!-- <button class="btn btn-default btn-follow"><i class="fa fa-star"></i> SUIVRE</button> -->
-			<?php if(  Authorisation::canEditItem( Yii::app()->session["userId"], $_GET["type"], (string)$_GET["id"])){?>
+			<?php if(  Authorisation::canEditItem( Yii::app()->session["userId"], $_GET["type"], (string)$_GET["id"]) || Yii::app()->session["userId"] == @$element["creator"] ){?>
 			<button class="btn btn-default" onclick='elementLib.editElement("<?php echo @$_GET["type"]; ?>","<?php echo (string)@$element["_id"]; ?>")' ><i class="fa fa-pencil text-red"></i> Edit</button>
 			<?php } ?>
 		</div>
