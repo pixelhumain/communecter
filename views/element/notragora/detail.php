43e392bc8ed734b11a43e6fd213a0fbffa4443a8
Sylvain Barbot <sylvain.barbot@gmail.com>
2017-05-16 16:22:53 +0400

    c'était addslashes qu'il fallait !

--- views/element/notragora/detail.php
+++ views/element/notragora/detail.php
@@ -654,7 +654,7 @@
   	var pois = <?php echo json_encode($entitiesPois); ?>
 
 	jQuery(document).ready(function() {
-		var elementName = "<?php echo htmlentities($element["name"])?>";
+		var elementName = "<?php echo addslashes($element["name"])?>";
 		setTitle("<span id='main-title-menu'>"+elementName+"</span>",elementName, elementName);
 		$("#changePasswordBtn").click(function () {
   			mylog.log("changePasswordbuttton");
