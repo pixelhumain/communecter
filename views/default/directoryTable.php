92d64df44a2138458dc647619da414b6570f62e0
LassalleRemi <remil.lassalle@gmail.com>
2017-07-12 16:40:14 +0200

    ajout date creation dans directory

--- views/default/directoryTable.php
+++ views/default/directoryTable.php
@@ -15,9 +15,9 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 
 <div class="panel panel-white">
 	<div class="panel-heading border-light">
-		<h4 class="panel-title"><i class="fa fa-globe fa-2x text-green"></i> <a href="javascript:;" onclick="applyStateFilter('organization|NGO|Group|LocalBusiness')" class="filter<?php echo Organization::COLLECTION ?> btn btn-xs btn-default"> Organizations <span class="badge badge-warning"> <?php echo count(@$organizations) ?></span></a> 
-																				<a href="javascript:;" onclick="applyStateFilter('person')" class="filter<?php echo Person::COLLECTION ?> btn btn-xs btn-default"> People <span class="badge badge-warning"> <?php echo count(@$people) ?></span></a>  
-																				<a href="javascript:;" onclick="applyStateFilter('event|concert|meeting|dance')" class="filter<?php echo Event::COLLECTION ?> btn btn-xs btn-default"> Events <span class="badge badge-warning"> <?php echo count(@$events) ?></span></a> 
+		<h4 class="panel-title"><i class="fa fa-globe fa-2x text-green"></i> <a href="javascript:;" onclick="applyStateFilter('organization|NGO|Group|LocalBusiness')" class="filter<?php echo Organization::COLLECTION ?> btn btn-xs btn-default"> Organizations <span class="badge badge-warning"> <?php echo count(@$organizations) ?></span></a>
+																				<a href="javascript:;" onclick="applyStateFilter('person')" class="filter<?php echo Person::COLLECTION ?> btn btn-xs btn-default"> People <span class="badge badge-warning"> <?php echo count(@$people) ?></span></a>
+																				<a href="javascript:;" onclick="applyStateFilter('event|concert|meeting|dance')" class="filter<?php echo Event::COLLECTION ?> btn btn-xs btn-default"> Events <span class="badge badge-warning"> <?php echo count(@$events) ?></span></a>
 																				<a href="javascript:;" onclick="applyStateFilter('project')" class="filter<?php echo Project::COLLECTION ?> btn btn-xs btn-default"> Projects <span class="badge badge-warning"> <?php echo count(@$projects) ?></span></a>
 																				<a href="javascript:;" onclick="clearAllFilters('')" class="btn btn-xs btn-default"> All</a></h4>
 	</div>
@@ -32,7 +32,7 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 
 	</div>
 	<div class="panel-body">
-		<div>	
+		<div>
 			<?php //var_dump($projects) ?>
 			<table class="table table-striped table-bordered table-hover  directoryTable">
 				<thead>
@@ -44,11 +44,12 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 						<?php }?>
 						<th>Tags</th>
 						<th>Scope</th>
+						<th>Date de création</th>
 						<th>Actions</th>
 					</tr>
 				</thead>
 				<tbody class="directoryLines">
-					<?php 
+					<?php
 					$memberId = Yii::app()->session["userId"];
 					$memberType = Person::COLLECTION;
 					$tags = array();
@@ -57,45 +58,45 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 						"codePostal"=>array(),
 						"region"=>array(),
 					);
-					
+
 					/* ************ ORGANIZATIONS ********************** */
-					if(isset($organizations)) 
-					{ 
-						foreach ($organizations as $e) 
-						{ 
+					if(isset($organizations))
+					{
+						foreach ($organizations as $e)
+						{
 							buildDirectoryLine($e, Organization::COLLECTION, Organization::CONTROLLER, Organization::ICON, $this->module->id,$tags,$scopes);
 						};
 					}
 
 					/* ********** PEOPLE ****************** */
-					if(isset($people)) 
-					{ 
-						foreach ($people as $e) 
-						{ 
+					if(isset($people))
+					{
+						foreach ($people as $e)
+						{
 							buildDirectoryLine($e, Person::COLLECTION, Person::CONTROLLER, Person::ICON, $this->module->id,$tags,$scopes);
 						}
 					}
 
 					/* ************ EVENTS ************************ */
-					if(isset($events)) 
-					{ 
-						foreach ($events as $e) 
-						{ 
+					if(isset($events))
+					{
+						foreach ($events as $e)
+						{
 							buildDirectoryLine($e, Event::COLLECTION, Event::CONTROLLER, Event::ICON, $this->module->id,$tags,$scopes);
 						}
 					}
-	
+
 					/* ************ PROJECTS **************** */
-					if( count($projects) ) 
-					{ 
-						foreach ($projects as $e) 
-						{ 
+					if( count($projects) )
+					{
+						foreach ($projects as $e)
+						{
 							buildDirectoryLine($e, Project::COLLECTION, Project::CONTROLLER, Project::ICON, $this->module->id,$tags,$scopes);
 						}
 					}
 
 					function buildDirectoryLine( $e, $collection, $type, $icon, $moduleId, &$tags, &$scopes ){
-							
+
 							if(!isset( $e['_id'] ) || !isset( $e["name"]) || $e["name"] == "" )
 								return;
 							$actions = "";
@@ -136,9 +137,9 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 									$actions .= '<li><a href="javascript:;" data-id="'.$id.'" data-type="'.$type.'" class="margin-right-5 deleteThisBtn"><i class="fa fa-times text-red"></i>Delete</a> </li>';
 									//TODO
 									$actions .= '<li><a href="javascript:;" data-id="'.$id.'" data-type="'.$type.'" class="margin-right-5 banThisBtn"><i class="fa fa-times text-red"></i> TODO : Ban</a> </li>';
-									
+
 								} else if( $type == Organization::CONTROLLER ) {
-								
+
 								}
 							}
 
@@ -148,19 +149,19 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 						$strHTML = '<tr id="'.$type.(string)$id.'">'.
 							'<td class="'.$collection.'Line '.$classes.'">'.
 								'<a href="'.Yii::app()->createUrl('/'.$moduleId.'/'.$type.'/dashboard/id/'.$id).'">';
-									if ($e && isset($e["imagePath"])){ 
+									if ($e && isset($e["imagePath"])){
 										$strHTML .= '<img width="50" height="50" alt="image" class="img-circle" src="'.Yii::app()->createUrl('/'.$moduleId.'/document/resized/50x50'.$e['imagePath']).'">'.((isset($e["type"])) ? $e["type"] : "");
-									} else { 
+									} else {
 										$strHTML .= '<i class="fa '.$icon.' fa-2x"></i> '.$type.'';
-									} 
+									}
 								$strHTML .= '</a>';
 							$strHTML .= '</td>';
-							
+
 							/* **************************************
 							* NAME
 							***************************************** */
 							$strHTML .= '<td><a href="'.Yii::app()->createUrl('/'.$moduleId.'/'.$type.'/dashboard/id/'.$id).'">'.((isset($e["name"]))? $e["name"]:"").'</a></td>';
-							
+
 							/* **************************************
 							* EMAIL for admin use only
 							***************************************** */
@@ -175,7 +176,7 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 							if(isset($e["tags"])){
 								foreach ($e["tags"] as $key => $value) {
 									$strHTML .= ' <a href="#" onclick="applyTagFilter(\''.$value.'\')"><span class="label label-inverse">'.$value.'</span></a>';
-									if( $tags != "" && !in_array($value, $tags) ) 
+									if( $tags != "" && !in_array($value, $tags) )
 										array_push($tags, $value);
 								}
 							}
@@ -187,33 +188,46 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 							$strHTML .= '<td>';
 							if( isset($e["address"]) && isset( $e["address"]['codeInsee']) ){
 								$strHTML .= ' <a href="#" onclick="applyScopeFilter('.$e["address"]['codeInsee'].')"><span class="label label-inverse">'.$e["address"]['codeInsee'].'</span></a>';
-								if( !in_array($e["address"]['codeInsee'], $scopes['codeInsee']) ) 
+								if( !in_array($e["address"]['codeInsee'], $scopes['codeInsee']) )
 									array_push($scopes['codeInsee'], $e["address"]['codeInsee'] );
 							}
 							if( isset($e["address"]) && isset( $e["address"]['codePostal']) ){
 								$strHTML .= ' <a href="#" onclick="applyScopeFilter('.$e["address"]['codePostal'].')"><span class="label label-inverse">'.$e["address"]['codePostal'].'</span></a>';
-								if( !in_array($e["address"]['codePostal'], $scopes['codePostal']) ) 
+								if( !in_array($e["address"]['codePostal'], $scopes['codePostal']) )
 									array_push($scopes['codePostal'], $e["address"]['codePostal'] );
 							}
 							if( isset($e["address"]) && isset( $e["address"]['region']) ){
 								$strHTML .= ' <a href="#" onclick="applyScopeFilter('.$e["address"]['region'].')"><span class="label label-inverse">'.$e["address"]['region'].'</span></a>';
-								if( !in_array($e["address"]['region'], $scopes['region']) ) 
+								if( !in_array($e["address"]['region'], $scopes['region']) )
 									array_push($scopes['region'], $e["address"]['region'] );
-							}	
+							}
 							$strHTML .= '</td>';
+
+							/* **************************************
+							* DATE DE CREATION
+							***************************************** */
+							$createDate = null;
+							if (isset($e["created"])) {
+								if($type == "organization"){
+									$createDate = date("Y-m-d",$e["created"]);
+								}else {
+									$createDate = date("Y-m-d", $e["created"]->sec);
+								}
+							}else $createDate = "";
+							$strHTML .= '<td><a href="'.Yii::app()->createUrl('/'.$moduleId.'/'.$type.'/dashboard/id/'.$id).'">'.$createDate.'</a></td>';
 
 							/* **************************************
 							* ACTIONS
 							***************************************** */
 							$strHTML .= '<td class="center">';
-							if( !empty($actions) && Yii::app()->session["userIsAdmin"] ) 
+							if( !empty($actions) && Yii::app()->session["userIsAdmin"] )
 								$strHTML .= '<div class="btn-group">'.
 											'<a href="#" data-toggle="dropdown" class="btn btn-red dropdown-toggle btn-sm"><i class="fa fa-cog"></i> <span class="caret"></span></a>'.
 											'<ul class="dropdown-menu pull-right dropdown-dark" role="menu">'.
 												$actions.
 											'</ul></div>';
 							$strHTML .= '</td>';
-						
+
 						$strHTML .= '</tr>';
 						echo $strHTML;
 					}
@@ -224,19 +238,19 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule);*/
 
 
 			<div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 3px; width: 0px; display: none;"><div class="ps-scrollbar-x" style="left: -10px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; right: 3px; height: 230px; display: inherit;"><div class="ps-scrollbar-y" style="top: 0px; height: 0px;"></div></div>
-			<?php 
+			<?php
 				/*if (isset($organizations) && count($organizations) == 0) {
 			?>
 				<div id="infoPodOrga" class="padding-10">
-					<blockquote> 
-						Create or Connect 
-						<br>an Organization, NGO,  
-						<br>Local Business, Informal Group. 
-						<br>Build links in your network, 
-						<br>to create a connected local directory 
+					<blockquote>
+						Create or Connect
+						<br>an Organization, NGO,
+						<br>Local Business, Informal Group.
+						<br>Build links in your network,
+						<br>to create a connected local directory
 					</blockquote>
 				</div>
-			<?php 
+			<?php
 				};*/
 			?>
 		</div>
@@ -256,11 +270,11 @@ jQuery(document).ready(function() {
 	resetDirectoryTable() ;
 	if(openingFilter != "")
 		$('.filter'+openingFilter).trigger("click");
-});	
+});
 
 
-function resetDirectoryTable() 
-{ 
+function resetDirectoryTable()
+{
 	mylog.log("resetDirectoryTable");
 
 	if( !$('.directoryTable').hasClass("dataTable") )
@@ -283,8 +297,8 @@ function resetDirectoryTable()
 			// set the initial value
 			"iDisplayLength" : 10,
 		});
-	} 
-	else 
+	}
+	else
 	{
 		if( $(".directoryLines").children('tr').length > 0 )
 		{
@@ -302,7 +316,7 @@ function applyStateFilter(str)
 	mylog.log("applyStateFilter",str);
 	directoryTable.DataTable().column( 0 ).search( str , true , false ).draw();
 }
-function clearAllFilters(str){ 
+function clearAllFilters(str){
 	directoryTable.DataTable().column( 0 ).search( str , true , false ).draw();
 	directoryTable.DataTable().column( 2 ).search( str , true , false ).draw();
 	directoryTable.DataTable().column( 3 ).search( str , true , false ).draw();
@@ -313,12 +327,12 @@ function applyTagFilter(str)
 	if(!str){
 		str = "";
 		sep = "";
-		$.each($(".btn-tag.active"), function() { 
+		$.each($(".btn-tag.active"), function() {
 			mylog.log("applyTagFilter",$(this).data("id"));
 			str += sep+$(this).data("id");
 			sep = "|";
 		});
-	} else 
+	} else
 		clearAllFilters("");
 	mylog.log("applyTagFilter",str);
 	directoryTable.DataTable().column( 2 ).search( str , true , false ).draw();
@@ -331,12 +345,12 @@ function applyScopeFilter(str)
 	if(!str){
 		str = "";
 		sep = "";
-		$.each( $(".btn-context-scope.active"), function() { 
+		$.each( $(".btn-context-scope.active"), function() {
 			mylog.log("applyScopeFilter",$(this).data("val"));
 			str += sep+$(this).data("val");
 			sep = "|";
 		});
-	} else 
+	} else
 		clearAllFilters("");
 	mylog.log("applyScopeFilter",str);
 	directoryTable.DataTable().column( 3 ).search( str , true , false ).draw();
@@ -345,14 +359,14 @@ function applyScopeFilter(str)
 
 function bindAdminBtnEvents(){
 	mylog.log("bindAdminBtnEvents");
-	
-	<?php 
+
+	<?php
 	/* **************************************
 	* ADMIN STUFF
 	***************************************** */
-	if( Yii::app()->session["userIsAdmin"] ) {?>		
+	if( Yii::app()->session["userIsAdmin"] ) {?>
 
-		$(".validateThisBtn").off().on("click",function () 
+		$(".validateThisBtn").off().on("click",function ()
 		{
 			mylog.log("validateThisBtn click");
 	        $(this).empty().html('<i class="fa fa-spinner fa-spin"></i>');
@@ -360,9 +374,9 @@ function bindAdminBtnEvents(){
 	        var id = $(this).data("id");
 	        var type = $(this).data("type");
 	        var urlToSend = baseUrl+"/"+moduleId+"/admin/activateuser/user/"+id;
-	        
+
 	        bootbox.confirm("confirm please !!",
-        	function(result) 
+        	function(result)
         	{
 				if (!result) {
 					btnClick.empty().html('<i class="fa fa-thumbs-down"></i>');
@@ -423,17 +437,17 @@ function bindAdminBtnEvents(){
 			});
 		});
 
-		
-		$(".switch2UserThisBtn").off().on("click",function () 
+
+		$(".switch2UserThisBtn").off().on("click",function ()
 		{
 			mylog.log("switch2UserThisBtn click");
 	        $(this).empty().html('<i class="fa fa-spinner fa-spin"></i>');
 	        var btnClick = $(this);
 	        var id = $(this).data("id");
 	        var urlToSend = baseUrl+"/"+moduleId+"/admin/switchto/uid/"+id;
-	        
+
 	        bootbox.confirm("confirm please !!",
-        	function(result) 
+        	function(result)
         	{
 				if (!result) {
 					btnClick.empty().html('<i class="fa fa-thumbs-down"></i>');
@@ -458,7 +472,7 @@ function bindAdminBtnEvents(){
 
 		});
 
-		$(".deleteThisBtn").off().on("click",function () 
+		$(".deleteThisBtn").off().on("click",function ()
 		{
 			mylog.log("deleteThisBtn click");
 	        $(this).empty().html('<i class="fa fa-spinner fa-spin"></i>');
@@ -466,9 +480,9 @@ function bindAdminBtnEvents(){
 	        var id = $(this).data("id");
 	        var type = $(this).data("type");
 	        var urlToSend = baseUrl+"/"+moduleId+"/admin/delete/type/"+type+"/id/"+id;
-	        
+
 	        bootbox.confirm("confirm please !!",
-        	function(result) 
+        	function(result)
         	{
 				if (!result) {
 					btnClick.empty().html('<i class="fa fa-thumbs-down"></i>');
@@ -492,9 +506,9 @@ function bindAdminBtnEvents(){
 			});
 
 		});
-	
+
 	<?php } ?>
-	$(".banThisBtn").off().on("click",function () 
+	$(".banThisBtn").off().on("click",function ()
 		{
 			mylog.log("banThisBtn click");
 		});
