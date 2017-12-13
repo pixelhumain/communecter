89b0cf336af86d8a68fd08ba46e909f4e297e04f
alexis <alexis@ckiw.me>
2017-12-13 14:17:48 +0100

    cleaning

--- docs/process.md
+++
@@ -1,9 +0,0 @@
-
-process Organization
-	New Organization
-		form.php => minimal information
-		/organization/savenew
-	Update Organization
-		view.php + editOrganization.php
-		more information and data
-		/organization/save
--- views/docs/network/iviatic.php
+++
@@ -1,5 +0,0 @@
-<span class="tpl_shortDesc">
-	<br/><br/>
-	La cartographie des acteurs de la mobilité est un outil visant à soutenir le développement de projets collaboratifs dans la région des Hauts-de-France. Elle vous permet d’identifier les partenaires et compétences dont vous avez besoin pour construire et renforcer votre projet.<br/><br/>
-L’équipe d’i-viaTIC se tient à votre disposition pour plus d’informations sur les structures représentées sur la carte et pour faciliter la mise en relation.<br/><br/>
-</span>  
